96e8e1fabf48847dd126c37f4dd5b3fd442f72db
SoJuSo <asdfg7123@gmail.com>
2024-02-07 16:40:29 +0900

    🔧chore : 경로별칭 설정
    Co-authored-by: Jaehyun Ahn <JaeHyunGround@users.noreply.github.com>
    Co-authored-by: kim-hyunjoo <kim-hyunjoo@users.noreply.github.com>
    Co-authored-by: Kyungbin <pkb8839@naver.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@stores': path.resolve(__dirname, './src/stores'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
   plugins: [react()],
+  build: {
+    sourcemap: false,
+  },
 });
