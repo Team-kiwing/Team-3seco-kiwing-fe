e0af6255295b86ac621dc3f10c3e3125b50514f0
SoJuSo <asdfg7123@gmail.com>
2024-02-08 19:36:34 +0900

    🐛fix : 라우터 에러 수정
    라우터 에러를 수정하였습니다.
    
    에러 원인 : 구조적인 문제가 있었습니다.
    BrowserRouter를 Route 폴더의 index.ts 내부에서 감싸고 있어서
    라우팅이 제대로 동작하지 않았습니다.
    
    해결 방법 : Main.tsx에서 사용 컴포넌트를 BrowserRouter로 감싸주고
    App.tsx에서 Router 컴포넌트만 출력하여 라우팅 처리를 진행하도록 구현하였습니다.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,51 +1,66 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
+import {
+  AuthPage,
+  HubPage,
+  MainPage,
+  MyListPage,
+  NotFoundPage,
+  RegisterPage,
+  ReportPage,
+  SharedItemPage,
+  SharedPage,
+  TestPage,
+} from '@/pages';
 
 const Router = () => {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          {/* 프레이머 모션 용 */}
-          <Route>
-            <Route
-              path="/" // 메인페이지
-              // element={<App />}
-            />
-            <Route
-              path="/auth" // 로그인, 회원가입 페이지
-              // element={<App />}
-            />
-            <Route
-              path="/register" // 회원가입 후 등록 페이지
-              // element={<App />}
-            />
-            <Route
-              path="/hub" // 질문 허브 페이지(검색)
-              // element={<PageOne />}
-            />
-            <Route
-              path="/shared" // 공유된 질문 페이지(검색)
-              // element={<PageTwo />}
-            />
-            <Route
-              path="/shared/:id" // 공유된 질문 페이지(별개의 페이지)
-              // element={<PageTwo />}
-            />
-            <Route
-              path="/@:id" // 내 질문 리스트, 이메일 주소로 개인 페이지
-              // element={<PageThree />}
-            />
-            <Route
-              path="/report" // 신고, 건의 페이지
-              // element={<NotFoundPage />}
-            />
-            <Route
-              path="*" // 404 페이지
-              // element={<NotFoundPage />}
-            />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        {/* 프레이머 모션 용 */}
+        <Route>
+          <Route
+            path="/" // 메인페이지
+            element={<MainPage />}
+          />
+          <Route
+            path="/auth" // 로그인, 회원가입 페이지
+            element={<AuthPage />}
+          />
+          <Route
+            path="/register" // 회원가입 후 등록 페이지
+            element={<RegisterPage />}
+          />
+          <Route
+            path="/hub" // 질문 허브 페이지(검색)
+            element={<HubPage />}
+          />
+          <Route
+            path="/shared" // 공유된 질문 페이지(검색)
+            element={<SharedPage />}
+          />
+          <Route
+            path="/shared/:id" // 공유된 질문 페이지(별개의 페이지)
+            element={<SharedItemPage />}
+          />
+          <Route
+            path="/@:id" // 내 질문 리스트, 이메일 주소로 개인 페이지
+            element={<MyListPage />}
+          />
+          <Route
+            path="/report" // 신고, 건의 페이지
+            element={<ReportPage />}
+          />
+          <Route
+            path="/test" // test 페이지
+            element={<TestPage />}
+          />
+          <Route
+            path="*" // 404 페이지
+            element={<NotFoundPage />}
+          />
+        </Route>
+      </Routes>
     </>
   );
 };
