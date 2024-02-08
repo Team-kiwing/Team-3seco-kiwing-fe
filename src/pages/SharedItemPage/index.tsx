441f2bfd3d10dd5785327eda783157375afeed8d
SoJuSo <asdfg7123@gmail.com>
2024-02-08 19:39:20 +0900

    ✨feat : 개별 파일에 어떤 페이지를 나타내는 지 기본 코드 작성
    개별 파일에 어떤 페이지를 나타내는 지 기본 코드 작성하였습니다.
    
    그리고 pages의 구조를 일부 변경하였습니다.
    
    변경한 이유
    pages/index.ts에서 export 구문을 병합해주는 경우 아래와 같이
    import 구문을 줄일 수 있습니다.
    import {
      AuthPage,
      ..중략
      TestPage,
    } from '@/pages';

---
+++ src/pages/SharedItemPage/index.tsx
@@ -0,0 +1,10 @@
+const SharedItemPage = () => {
+  // 요기다가 하시면 됩니다
+  return (
+    <>
+      <div>SharedItemPage 입니다.</div>
+    </>
+  );
+};
+
+export default SharedItemPage;
---
+++ src/pages/index.ts
@@ -0,0 +1,10 @@
+export { default as AuthPage } from './AuthPage';
+export { default as HubPage } from './HubPage';
+export { default as MainPage } from './MainPage';
+export { default as MyListPage } from './MyListPage';
+export { default as NotFoundPage } from './NotFoundPage';
+export { default as RegisterPage } from './RegisterPage';
+export { default as ReportPage } from './ReportPage';
+export { default as SharedItemPage } from './SharedItemPage';
+export { default as SharedPage } from './SharedPage';
+export { default as TestPage } from './TestPage';
