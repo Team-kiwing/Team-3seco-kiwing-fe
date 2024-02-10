6d1165d1c26575298f015b27c80829fb461c8550
Kyungbin <pkb8839@naver.com>
2024-02-10 18:21:17 +0900

    ✨feat : Box Skeleton 컴포넌트 구현

---
+++ src/components/common/Skeleton/Box.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import { Base } from './Base';
+import { PropsBoxSkeleton } from './Skeleton.type';
+
+export const Box = styled(Base)<PropsBoxSkeleton>`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  border-radius: ${(props) => props.borderRadius};
+`;
