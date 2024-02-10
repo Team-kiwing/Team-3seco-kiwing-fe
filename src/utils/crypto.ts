f01b335499df70b53c696a12ebe5f0f5af05b431
SoJuSo <asdfg7123@gmail.com>
2024-02-11 00:25:16 +0900

    ✨feat : 암복호화 방어코드 추가
    암복호화를 진행하는 로직에서 빈 문자열을 입력할 경우
    예외처리하도록 수정하였습니다.

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -2,10 +2,12 @@ const { VITE_CRYPTO_KEY } = import.meta.env;
 import CryptoJS, { AES } from 'crypto-js';
 
 export const aesEncrypt = (data: string) => {
+  if (!data) throw new Error('데이터에 빈 문자열이 들어옵니다!!');
   return AES.encrypt(data, VITE_CRYPTO_KEY).toString();
 };
 
 export const aesDecrypt = (data: string) => {
+  if (!data) throw new Error('데이터에 빈 문자열이 들어옵니다!!');
   const bytes = AES.decrypt(data, VITE_CRYPTO_KEY);
   const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
 
