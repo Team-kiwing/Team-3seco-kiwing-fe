ab67ca52f4cb02cda9b2c865ced20b01cb450c89
SoJuSo <asdfg7123@gmail.com>
2024-02-11 00:29:01 +0900

    ✨feat : 반환 타입 지정
    함수 반환 타입 string을 함수에 리턴값에 명시하였습니다.

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,12 +1,12 @@
 const { VITE_CRYPTO_KEY } = import.meta.env;
 import CryptoJS, { AES } from 'crypto-js';
 
-export const aesEncrypt = (data: string) => {
+export const aesEncrypt = (data: string): string => {
   if (!data) throw new Error('데이터에 빈 문자열이 들어옵니다!!');
   return AES.encrypt(data, VITE_CRYPTO_KEY).toString();
 };
 
-export const aesDecrypt = (data: string) => {
+export const aesDecrypt = (data: string): string => {
   if (!data) throw new Error('데이터에 빈 문자열이 들어옵니다!!');
   const bytes = AES.decrypt(data, VITE_CRYPTO_KEY);
   const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
