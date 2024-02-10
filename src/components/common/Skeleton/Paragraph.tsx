4d3fc74614af5fc81de4329e7fb00939f980ef3f
Kyungbin <pkb8839@naver.com>
2024-02-10 18:22:51 +0900

    ✨feat : Paragraph Skeleton 컴포넌트 구현
    마지막 문단의 길이는 65% 주었습니다.

---
+++ src/components/common/Skeleton/Paragraph.tsx
@@ -0,0 +1,30 @@
+import { Box } from './Box';
+import { PropsParagraphSkeleton } from './Skeleton.type';
+
+export const Paragraph = ({
+  line = 3,
+  width,
+  height,
+  borderRadius,
+  marginBottom = '1rem',
+}: PropsParagraphSkeleton) => {
+  return (
+    <div>
+      {Array.from(Array(line), (_, index) => {
+        return (
+          <Box
+            key={index}
+            width={width}
+            height={height}
+            borderRadius={borderRadius}
+            style={{
+              marginBottom: marginBottom,
+              width:
+                index === line - 1 ? `${parseInt(width) * 0.65}rem` : width,
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+};
