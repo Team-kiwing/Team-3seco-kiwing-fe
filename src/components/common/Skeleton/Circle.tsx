21e56f53d772aae92c41df123f1128c2739bbb75
Kyungbin <pkb8839@naver.com>
2024-02-10 18:22:15 +0900

    ✨feat : Circle Skeleton 컴포넌트 구현

---
+++ src/components/common/Skeleton/Circle.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import { Base } from './Base';
+import { PropsCircleSkeleton } from './Skeleton.type';
+
+export const Circle = styled(Base)<PropsCircleSkeleton>`
+  width: ${({ size }) => size};
+  height: ${({ size }) => size};
+  border-radius: 50%;
+`;
