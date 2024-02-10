c31bb4df227288678c0f3f7efa02b2873126ef23
Kyungbin <pkb8839@naver.com>
2024-02-10 18:24:15 +0900

    ✨feat : Skeleton 컴포넌트 타입 정의
    - BoxSkeleton 타입 정의
    - CircleSkeleton 타입 정의
    - ParagraphSkeleton 타입 정의

---
+++ src/components/common/Skeleton/Skeleton.type.ts
@@ -0,0 +1,17 @@
+export interface PropsBoxSkeleton {
+  width: string;
+  height: string;
+  borderRadius?: string;
+}
+
+export interface PropsCircleSkeleton {
+  size: string;
+}
+
+export interface PropsParagraphSkeleton extends React.ComponentProps<'div'> {
+  width: string;
+  height: string;
+  line?: number;
+  borderRadius?: string;
+  marginBottom?: string;
+}
