22c7c036fdf0e9926f5c0bef93186cbbb5498e59
Kyungbin <pkb8839@naver.com>
2024-02-12 01:33:45 +0900

    📝docs : jsDoc 작성
    jsDoc을 통해 뱃지컴포넌트 사용법을 설명하였습니다.

--- src/components/common/Badge/Badge.tsx
+++ src/components/common/Badge/Badge.tsx
@@ -3,6 +3,21 @@ import { PropsWithChildren } from 'react';
 import { StyledBadge } from './Badge.style';
 import { PropsBadge } from './Badge.type';
 
+/**
+ * @summary 사용법 <Badge
+      size={'m'}
+      state={basic}
+      hover={true}
+      isActive={false}
+    />
+ * @description 공통 Badge 컴포넌트
+ * @param size font-size의 크기
+ * @param state basic과 focus가 있으면 background-color와 color의 색깔을 지정합니다.
+ * @param hover true false에 따라 hover가 적용됩니다.
+ * @param isActive true false에 따라 state 즉, background-color와 color가 변합니다.
+ * @returns
+ */
+
 const Badge = ({
   $state,
   children,
