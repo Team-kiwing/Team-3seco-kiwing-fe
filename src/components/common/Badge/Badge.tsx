cb6fdf275ef19df0f92a8622fb89f2f456a6d956
Kyungbin <pkb8839@naver.com>
2024-02-12 01:22:06 +0900

    ✨feat : Badge 공통 컴포넌트 구현
    - 총 4가지의 프롭스(size, state, hover, isActive)를 받습니다.
    - state, size는 필수입니다.

--- src/components/common/Badge/Badge.tsx
+++ src/components/common/Badge/Badge.tsx
@@ -1,5 +1,27 @@
-const Badge = () => {
-  return <></>;
+import { PropsWithChildren } from 'react';
+
+import { StyledBadge } from './Badge.style';
+import { PropsBadge } from './Badge.type';
+
+const Badge = ({
+  $state,
+  children,
+  $hover,
+  $isActive,
+  $size,
+  ...props
+}: PropsWithChildren<PropsBadge>) => {
+  return (
+    <StyledBadge
+      $size={$size}
+      $state={$state}
+      $hover={$hover}
+      $isActive={$isActive}
+      {...props}
+    >
+      {children}
+    </StyledBadge>
+  );
 };
 
 export default Badge;
