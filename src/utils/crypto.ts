a6b064310a28bdd5eb5c5be499ddfa076ffc49c8
SoJuSo <asdfg7123@gmail.com>
2024-02-11 23:31:04 +0900

    🐛fix : if문 구조 수정
    crypto 암,복호화 함수의 구문을 코딩 컨벤션에 맞도록
    if else문으로 수정하였습니다.

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -2,14 +2,20 @@ const { VITE_CRYPTO_KEY } = import.meta.env;
 import CryptoJS, { AES } from 'crypto-js';
 
 export const aesEncrypt = (data: string): string => {
-  if (!data) throw new Error('데이터에 빈 문자열이 들어옵니다!!');
-  return AES.encrypt(data, VITE_CRYPTO_KEY).toString();
+  if (!data) {
+    throw new Error('데이터에 빈 문자열이 들어옵니다!!');
+  } else {
+    return AES.encrypt(data, VITE_CRYPTO_KEY).toString();
+  }
 };
 
 export const aesDecrypt = (data: string): string => {
-  if (!data) throw new Error('데이터에 빈 문자열이 들어옵니다!!');
-  const bytes = AES.decrypt(data, VITE_CRYPTO_KEY);
-  const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+  if (!data) {
+    throw new Error('데이터에 빈 문자열이 들어옵니다!!');
+  } else {
+    const bytes = AES.decrypt(data, VITE_CRYPTO_KEY);
+    const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
 
-  return decryptedData;
+    return decryptedData;
+  }
 };
