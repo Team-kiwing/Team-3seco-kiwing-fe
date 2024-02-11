dd3f45bb8dca4c1e8700ac90428fa90b98f264f0
SoJuSo <asdfg7123@gmail.com>
2024-02-12 00:01:46 +0900

    ♻️refactor : sizeMapping 코드 구조 개선
    sizeMapping 객체를 추가하여 코드 라인을 축소, 코드 가독성을 개선하였습니다.
    또한 추후 확장에도 용이할 것으로 생각됩니다.

--- src/components/common/IconWrapper/IconWrapper.tsx
+++ src/components/common/IconWrapper/IconWrapper.tsx
@@ -1,5 +1,15 @@
 import styled, { css } from 'styled-components';
 
+const sizeMapping = {
+  xss: '10px',
+  xs: '20px',
+  s: '30px',
+  m: '40px',
+  l: '50px',
+  xl: '60px',
+  xxl: '70px',
+};
+
 const IconWrapper = styled.div<{
   /**
    * @summary 사용될 아이콘의 사이즈를 결정합니다.
@@ -27,56 +37,21 @@ const IconWrapper = styled.div<{
         height: ${$size}px;
       `;
     } else {
-      switch ($size) {
-        case 'xss':
-          return css`
-            width: 10px;
-            height: 10px;
-          `;
-        case 'xs':
-          return css`
-            width: 20px;
-            height: 20px;
-          `;
-        case 's':
-          return css`
-            width: 30px;
-            height: 30px;
-          `;
-        case 'm':
-          return css`
-            width: 40px;
-            height: 40px;
-          `;
-        case 'l':
-          return css`
-            width: 50px;
-            height: 50px;
-          `;
-        case 'xl':
-          return css`
-            width: 60px;
-            height: 60px;
-          `;
-        case 'xxl':
-          return css`
-            width: 70px;
-            height: 70px;
-          `;
-        default:
-          return css`
-            width: 40px;
-            height: 40px;
-          `;
-      }
+      const sizeValue = sizeMapping[$size] || '40px'; // Default size
+      return css`
+        width: ${sizeValue};
+        height: ${sizeValue};
+      `;
     }
   }}
+
   svg {
     cursor: pointer;
     width: 100%;
     height: 100%;
     fill: ${(props) => props.$fillColor};
   }
+
   :hover {
     fill: ${(props) => props.$hoverIconColor};
   }
