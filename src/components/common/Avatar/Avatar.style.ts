0e5213d1133df0aab0e4c52bd0fa7cd57c252ceb
JaeHyunGround <gothddlek@naver.com>
2024-02-12 21:55:44 +0900

    ✨feat : Avatar 스타일 제작
    Avatar 컴포넌트의 스타일을 제작했습니다.
    
    - 모바일 환경은 5rem (50px), pc 환경은 10rem (100px)로 잡았습니다.

---
+++ src/components/common/Avatar/Avatar.style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+import { AvatarProps } from './Avatar.type';
+
+const AvaterSize: { [key: string]: string } = {
+  mobile: '5rem',
+  pc: '10rem',
+};
+
+export const AvatarWrapper = styled.div<AvatarProps>`
+  position: relative;
+  display: inline-block;
+  overflow: hidden;
+  width: ${({ $size }) => AvaterSize[$size]};
+  height: ${({ $size }) => AvaterSize[$size]};
+  background-image: ${({ $src }) =>
+    $src ? `url(${$src})` : 'url(src/assets/profile.png)'};
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  border-radius: 50%;
+`;
