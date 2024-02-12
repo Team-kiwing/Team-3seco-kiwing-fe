67dfa1a5a6fa3d4bbf0b2c0c1f0f172a5fb859d7
Kyungbin <pkb8839@naver.com>
2024-02-12 16:38:46 +0900

    📝docs : jsDoc 수정
    Skeleton 리펙하면서 사용법 또한 바뀌어서 다시 작성하였습니다.

--- src/components/common/Skeleton/Skeleton.tsx
+++ src/components/common/Skeleton/Skeleton.tsx
@@ -9,29 +9,30 @@ const Skeleton = {
 };
 /**
  * @summary 사용법 <Skeleton.Box
-        width="30rem"
-        height="10rem"
+        $width={718}
+        $height={63}
+        $borderRadius={15}
       />
-      <Skeleton.Circle size="10r" />
+      <Skeleton.Circle size={100} />
       <Skeleton.Paragraph
-        width="40rem"
-        height="2rem"
-        marginBottom="0.3rem"
-        line={6}
+        $width={625}
+        $height={42}
+        $marginBottom={10}
+        $line={6}
       />
  * @description 공통 Skeleton 컴포넌트
  * Box
- * @param width width
- * @param height height
- * @param borderRadius optional) borderRadius 테두리 둥글기, 기본 값: 4px
+ * @param $width width
+ * @param $height height
+ * @param $borderRadius optional) borderRadius 테두리 둥글기, 기본 값: 4px
  * Circle
- * @param size size
+ * @param $size size
  * Paragraph
- * @param width width
- * @param height height
- * @param line optional) line 문단의 개수, 기본 개수: 3
- * @param borderRadius optional) borderRadius 테두리 둥글기, 기본 값: 4px
- * @param marginBottom optional) marginBottom 문단 사이의 간격, 기본 값: 10px
+ * @param $width width
+ * @param $height height
+ * @param $line optional) line 문단의 개수, 기본 개수: 3
+ * @param $borderRadius optional) borderRadius 테두리 둥글기, 기본 값: 4px
+ * @param $marginBottom optional) marginBottom 문단 사이의 간격, 기본 값: 10px
  * @returns
  */
 export default Skeleton;
