a4a0b8b58a65c60b676abaa06ea52945fe2c3433
JaeHyunGround <gothddlek@naver.com>
2024-02-12 22:12:14 +0900

    ✨feat : Avatar 컴포넌트 구현
    Avatar 컴포넌트를 구현했습니다.

--- src/components/common/Avatar/Avatar.tsx
+++ src/components/common/Avatar/Avatar.tsx
@@ -1,5 +1,14 @@
-const Avatar = () => {
-  return <div></div>;
+import { AvatarWrapper } from './Avatar.style';
+import { AvatarProps } from './Avatar.type';
+
+const Avatar = ({ $size, $src, ...props }: AvatarProps) => {
+  return (
+    <AvatarWrapper
+      $size={$size}
+      $src={$src}
+      {...props}
+    />
+  );
 };
 
 export default Avatar;
