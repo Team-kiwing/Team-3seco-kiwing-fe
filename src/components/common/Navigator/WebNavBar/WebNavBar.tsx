ad9afc8a625fa9288f0c37f3caaa74e068b9d43a
SoJuSo <asdfg7123@gmail.com>
2024-02-12 19:45:19 +0900

    ✨feat : 기본 동작 구현
    기본 레이아웃과 hook을 이용한 WebNavBar 기본 동작을 구현하였습니다.

--- src/components/common/Navigator/WebNavBar/WebNavBar.tsx
+++ src/components/common/Navigator/WebNavBar/WebNavBar.tsx
@@ -1,3 +1,38 @@
-const WebNavBar = () => {};
+import { useMenu } from './WebNavBar.hook';
+import {
+  WebNavBarMyPage,
+  WebNavBarRouter,
+  WebNavBarWrapper,
+} from './WebNavBar.style';
+
+const WebNavBar = () => {
+  const {
+    isLogin,
+    handleLogo,
+    handleHub,
+    handleList,
+    handleMyList,
+    handleLogin,
+  } = useMenu();
+
+  console.log(isLogin);
+
+  return (
+    <WebNavBarWrapper>
+      <WebNavBarRouter>
+        <div onClick={handleLogo}>handleLogo</div>
+        <div onClick={handleHub}>handleHub</div>
+        <div onClick={handleList}>handleList</div>
+      </WebNavBarRouter>
+      <WebNavBarMyPage>
+        {isLogin ? (
+          <div onClick={() => handleMyList(':id')}>handleMyList</div>
+        ) : (
+          <div onClick={handleLogin}>Todo/LoginButton</div>
+        )}
+      </WebNavBarMyPage>
+    </WebNavBarWrapper>
+  );
+};
 
 export default WebNavBar;
