cbbe5ccfd1c5d8acbd7fe294bc7508cd37dfdb24
SoJuSo <asdfg7123@gmail.com>
2024-02-12 19:44:29 +0900

    💄design : WebNavBar default 레이아웃 생성
    피그마를 기반으로 한 기반 레이아웃을 생성하였습니다.

--- src/components/common/Navigator/WebNavBar/WebNavBar.style.tsx
+++ src/components/common/Navigator/WebNavBar/WebNavBar.style.tsx
@@ -1,1 +1,20 @@
-// style.tsx
+import styled from 'styled-components';
+
+import { Row } from '@/styles/globalStyles';
+
+export const WebNavBarWrapper = styled(Row)`
+  max-width: 1200px;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: space-between;
+
+  font-size: 3rem;
+`;
+
+export const WebNavBarRouter = styled(Row)`
+  gap: 1rem;
+`;
+
+export const WebNavBarMyPage = styled(Row)`
+  gap: 1rem;
+`;
