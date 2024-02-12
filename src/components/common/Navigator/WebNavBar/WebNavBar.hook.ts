73dd7eabc3bfee26d52112d5975ac4f120856471
SoJuSo <asdfg7123@gmail.com>
2024-02-12 19:43:22 +0900

    ✨feat : WebNavBar에서 사용되는 hook 정의
    피그마를 기반으로 사용되는 훅을 정의합니다.
    
    router 기반의 함수가 대부분이고
    로그인은 추후에 login 판별 함수가 생성되는 대로 추가 예정입니다.

--- src/components/common/Navigator/WebNavBar/WebNavBar.hook.ts
+++ src/components/common/Navigator/WebNavBar/WebNavBar.hook.ts
@@ -1,1 +1,30 @@
-// hook.ts
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const useMenu = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    // Todo 로그인 여부
+    // const id = getId();
+    // if (id) setIsLogin(true);
+    setIsLogin(false);
+    // setIsLogin(true);
+  }, []);
+
+  const handleLogo = () => navigator('/');
+  const handleHub = () => navigator('/hub');
+  const handleList = () => navigator('/shared');
+  const handleMyList = (userId: string): void => navigator(`/@${userId}`);
+  const handleLogin = () => navigator('/auth');
+
+  return {
+    isLogin,
+    handleLogo,
+    handleHub,
+    handleList,
+    handleMyList,
+    handleLogin,
+  };
+};
