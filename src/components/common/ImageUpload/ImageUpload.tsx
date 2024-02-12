ca0d0bf246cca8ca78c3542894bc32e0d5013505
Kyungbin <pkb8839@naver.com>
2024-02-12 21:48:52 +0900

    ✨feat : ImageUpload 컴포넌트 구현

--- src/components/common/ImageUpload/ImageUpload.tsx
+++ src/components/common/ImageUpload/ImageUpload.tsx
@@ -1,5 +1,43 @@
-const ImageUpload = () => {
-  return <div>이미지 업로드</div>;
+import { useRef } from 'react';
+import { PropsWithChildren } from 'react';
+
+import { Input } from './ImageUpload.style';
+import { PropsImageUpload } from './ImageUpload.type';
+
+const ImageUpload = ({
+  children,
+  onImageFile,
+  onImageFileUrl,
+}: PropsWithChildren<PropsImageUpload>) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+
+    if (selectedFile) {
+      const fileURL = URL.createObjectURL(selectedFile);
+      onImageFile(selectedFile);
+      onImageFileUrl(fileURL);
+    } else {
+      console.log('No file selected');
+    }
+  };
+
+  const handleChooseFile = () => {
+    inputRef.current?.click();
+  };
+
+  return (
+    <div onClick={handleChooseFile}>
+      <Input
+        onChange={handleChangeFile}
+        ref={inputRef}
+        type="file"
+        accept="image/*"
+      />
+      {children}
+    </div>
+  );
 };
 
 export default ImageUpload;
