4bcc675220c958df6ef33b246798e97d95929d51
JaeHyunGround <gothddlek@naver.com>
2024-02-13 20:36:51 +0900

    🐛fix : Avatar border 추가
    Avatar 컴포넌트에 border를 추가했습니다. (#D9D9D9, gray.100)
    
    - 화이트 모드에서 흰색 배경 프로필 이미지를 할 경우, 배경과 구분이 되지 않는 것을 방지합니다.

--- src/components/common/Avatar/Avatar.style.ts
+++ src/components/common/Avatar/Avatar.style.ts
@@ -14,10 +14,11 @@ export const AvatarWrapper = styled.div<AvatarProps>`
   overflow: hidden;
   width: ${({ $size }) => AvaterSize[$size]};
   height: ${({ $size }) => AvaterSize[$size]};
+  border: 0.1rem solid #d9d9d9; /* TODO: #d9d9d9 => gray.100 으로 바꾸기 */
+  border-radius: 50%;
   background-image: ${({ $src }) =>
     $src ? `url(${$src})` : 'url(src/assets/profile.png)'};
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
-  border-radius: 50%;
 `;
