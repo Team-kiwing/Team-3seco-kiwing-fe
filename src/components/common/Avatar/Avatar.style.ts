80d6ff0174b7ebc24834bcc070ffd1d9301b1453
JaeHyunGround <gothddlek@naver.com>
2024-02-14 00:09:33 +0900

    ♻️refactor : 프로필 기본 이미지 경로 상수화
    프로필 기본 이미지 경로를 상수화 했습니다.
    
    - Avatar > Avatar.const.ts 파일에 DEFAULT_PROFILE_IMAGE 이름으로 저장했습니다.

--- src/components/common/Avatar/Avatar.style.ts
+++ src/components/common/Avatar/Avatar.style.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+import { DEFAULT_PROFILE_IMAGE } from './Avatar.const';
 import { AvatarProps } from './Avatar.type';
 
 const AvaterSize: { [key: string]: string } = {
@@ -17,7 +18,7 @@ export const AvatarWrapper = styled.div<AvatarProps>`
   border: 0.1rem solid #d9d9d9; /* TODO: #d9d9d9 => gray.100 으로 바꾸기 */
   border-radius: 50%;
   background-image: ${({ $src }) =>
-    $src ? `url(${$src})` : 'url(src/assets/profile.png)'};
+    $src ? `url(${$src})` : `url(${DEFAULT_PROFILE_IMAGE})`};
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
