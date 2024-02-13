49639b64e716fe40c05ee2735b1e1e95248822e0
JaeHyunGround <gothddlek@naver.com>
2024-02-13 14:58:30 +0900

    🐛fix : Avatar 컴포넌트 jsDoc 수정

--- src/components/common/Avatar/Avatar.tsx
+++ src/components/common/Avatar/Avatar.tsx
@@ -6,7 +6,7 @@ import { AvatarProps } from './Avatar.type';
  * @description 공통 ImageUpload 컴포넌트
  * @summary 사용법 1) :  <Avatar $size="mobile" /> (사용자가 설정한 프로필 사진이 없을 때)
  * @summary 사용법 2) : <Avatar $size="pc" $src="/kiwing_square_green.png" /> (사용자가 설정한 프로필 사진이 있을 땐, 이미지 경로를 기입)
- * @param $size : Avatar 컴포넌트 사이즈 (pc, mobile로 기입)
+ * @param $size : Avatar 컴포넌트 사이즈 (pc, mobile, nav로 기입)
  * @param $src : 이미지 경로 (선택 사항)
  * @param...props : 커스텀을 위함
  * @returns
