d20dd093a8922b232feb7c05e6bfd121524fd5fc
SoJuSo <asdfg7123@gmail.com>
2024-02-15 00:25:58 +0900

    ✨feat : 모바일 하단 내비게이션 바 훅 구현

--- src/components/common/Navigator/MobileBottomNavBar/MobileBottomNavBar.hook.ts
+++ src/components/common/Navigator/MobileBottomNavBar/MobileBottomNavBar.hook.ts
@@ -1,1 +1,42 @@
-//hook.ts
+// 이 부분 로직이 WebNavBar.hook이랑 겹쳐서 상위 Navigator 폴더 훅으로 마이그레이션 예정입니다.
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+export const useNavigatorMenu = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const location = useLocation();
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    // Todo 로그인 여부
+    // const id = getId();
+    // if (id) setIsLogin(true);
+    // setIsLogin(false);
+    setIsLogin(true);
+  }, []);
+
+  const handleNavigate = (path: string): void => {
+    console.log(`clicked`);
+    if (location.pathname === path) {
+      return;
+    } else {
+      navigator(path);
+    }
+  };
+
+  const handleLogo = () => handleNavigate('/');
+  const handleHub = () => handleNavigate('/hub');
+  const handleSharedList = () => handleNavigate('/shared');
+  const handleLogin = () => handleNavigate('/auth');
+  const handleMyList = (userId: string) => handleNavigate(`/@${userId}`);
+
+  return {
+    isLogin,
+    location,
+    handleLogo,
+    handleHub,
+    handleSharedList,
+    handleMyList,
+    handleLogin,
+  };
+};
