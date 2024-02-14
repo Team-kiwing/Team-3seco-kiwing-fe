555a8f2bf934f2282aa1a1e64e1851bbc8dfb221
Kyungbin <pkb8839@naver.com>
2024-02-14 11:33:23 +0900

    [#19] ImageUpload 컴포넌트 v1.0.0  (#20)
    * ✨feat : ImageUpload 컴포넌트 뼈대 파일 생성
    
    * ✨feat : ImageUpload 타입 정의
    
    * 💄design : ImageUpload 스타일링
    
    display: hidden 을 주었습니다.
    
    * ✨feat : ImageUpload 컴포넌트 구현
    
    * 📝docs : jsDoc 작성
    
    ImageUpload의 사용법 jsDoc으로 작성하였습니다.
    
    * ✨feat : ImageUpload props 확장
    
    ImageUpload 전달되는 props들을 확장하였습니다.
    
    * 🐛fix : (정적)이미지만 업로드 가능하게 수정
    
    - 정적이미지만 업로드 가능하게 구현하였습니다.
    - jsDoc을 수정하였습니다.
    
    * 💄design : ImageUpload 스타일 추가
    
    ImageUpload에 ImageWrapper를 추가했습니다.
    - display: inline-block, border-radius: 50% 주었습니다.
    
    * 🐛fix : webp 이미지 파일 업로드 방지
    
    webp이미지 파일 업로드를 제한합니다.(동적인 이미지도 있기에)

---
+++ src/components/common/ImageUpload/ImageUpload.style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  display: none;
+`;
+
+export const ImageWrapper = styled.div`
+  display: inline-block;
+  border-radius: 50%;
+`;
