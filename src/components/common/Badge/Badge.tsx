a6fb0ad874047b5c278bc37fc35ffa9b2147bb1d
Kyungbin <pkb8839@naver.com>
2024-02-14 11:22:07 +0900

    📝docs : jsDoc 수정
    뱃지 컴포넌트가 사용법이 바껴서 다시 jsDoc을 작성하였습니다.

--- src/components/common/Badge/Badge.tsx
+++ src/components/common/Badge/Badge.tsx
@@ -3,16 +3,16 @@ import { PropsBadge } from './Badge.type';
 
 /**
  * @summary 사용법 <Badge
-      size={'m'}
-      state={basic}
-      hover={true}
-      isActive={false}
+      $size={'m'}
+      $state="basic"
+      $isHover={true}
+      $text={'백엔드'}
     />
  * @description 공통 Badge 컴포넌트
- * @param size font-size의 크기
- * @param state basic, focus, hot이 있으면 background-color와 color의 색깔을 지정합니다.
- * @param hover true false에 따라 hover가 적용됩니다.
- * @param isActive true false에 따라 state 즉, background-color와 color가 변합니다.
+ * @param $size font-size의 크기
+ * @param $state basic, focus, hashTag가 있으면 background-color와 color의 색깔을 지정합니다.
+ * @param $hover true false에 따라 hover가 적용됩니다.
+ * @param $text 뱃지안에 들어간 text값 입니다.
  * @returns
  */
 
