ba83331079b15cca9f2870e04d44e85e69b6927f
Kyungbin <pkb8839@naver.com>
2024-02-15 13:23:44 +0900

    ✨feat : fontWeight 및 borderRadius 상수화

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,3 +12,14 @@ export const DROP_DOWN = 3;
 export const MODAL_BACKGROUND = 4;
 export const MODAL_LAYOUT = 5;
 export const LOADING_BAR = 6;
+
+// BORDER //
+export const BORDER_WEB = 10;
+export const BORDER_MOBILE = 6;
+
+// FONT WEIGHT //
+export const FONT_LIGHT = 250;
+export const FONT_REGULAR = 400;
+export const FONT_SEMI_BOLD = 550;
+export const FONT_BOLD = 700;
+export const FONT_EXTRA_BOLD = 850;
