77a42928e3e128b6be88fd4dc060b03f5474bf6b
JaeHyunGround <gothddlek@naver.com>
2024-02-15 18:12:38 +0900

    ✨feat : Input 컴포넌트에 쓰일 타입 선언
    Input 컴포넌트에 쓰일 타입들을 선언했습니다.

---
+++ src/components/common/Input/Input.type.ts
@@ -0,0 +1,14 @@
+export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
+  width: string;
+  fontSize: number;
+  margin?: number;
+  label?: string;
+  placeholder?: string;
+  errorMessage?: string;
+}
+
+export interface InputStyledProps {
+  $width?: string;
+  $fontSize?: number;
+  $margin?: number;
+}
