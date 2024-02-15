52fef180361824e10a0c66976733d6e14ef55788
JaeHyunGround <gothddlek@naver.com>
2024-02-15 18:16:57 +0900

    ✨feat : Input 컴포넌트 구현
    Input 컴포넌트를 구현했습니다.
    
    - InputWrapper 자식으로 Label, StyledInput, ErrorMessage가 배치됩니다.

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,5 +1,24 @@
-const Input = () => {
-  return <div>인풋 컴포넌트</div>;
+import { ErrorMessage, InputWrapper, Label, StyledInput } from './Input.style';
+import { InputProps } from './Input.type';
+
+const Input = ({
+  width,
+  fontSize,
+  margin,
+  label,
+  placeholder,
+  errorMessage,
+}: InputProps) => {
+  return (
+    <InputWrapper
+      $width={width}
+      $fontSize={fontSize}
+    >
+      <Label $margin={margin}>{label}</Label>
+      <StyledInput placeholder={placeholder} />
+      <ErrorMessage $margin={margin}>{errorMessage}</ErrorMessage>
+    </InputWrapper>
+  );
 };
 
 export default Input;
