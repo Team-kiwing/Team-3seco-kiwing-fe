458036ad31aa6ac8869698ca0ccd53b76f8cda0e
JaeHyunGround <gothddlek@naver.com>
2024-02-15 18:53:31 +0900

    ✨feat : Input 컴포넌트 jsDoc 작성
    Input 컴포넌트에 jsDoc를 작성했습니다.

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,6 +1,28 @@
 import { ErrorMessage, InputWrapper, Label, StyledInput } from './Input.style';
 import { InputProps } from './Input.type';
 
+/**
+ *
+ * @description 공통 Input 컴포넌트
+ * @summary 사용법 :
+ * <Input
+        width={'50rem'}
+        fontSize={2}
+        margin={1.5}
+        label={'라벨이다'}
+        placeholder={'닉네임 써라'}
+        errorMessage={'에러다'}
+      />
+ * @param width : 필수 | string 타입. input의 너비를 의미함. (px or rem or % 로 기입)
+ * @param fontSize : 선택 | number 타입. 라벨, input, 에러 메시지의 폰트 사이즈를 의미. 생략 시 기본 값으로 2가 설정. (단위는 rem)
+ * @param margin : 선택 | number 타입. 라벨, input, 에러 메시지 간 간격을 의미. 생략 시 기본 값으로 1.5가 설정. (단위는 rem)
+ * @param label : 선택 | string 타입. input 위에 붙는 라벨 역할.
+ * @param placeholder : 선택 | string 타입. input의 placeholder 역할.
+ * @param errorMessage : 선택 | string 타입. input의 placeholder 역할.
+ * @param ...props : 커스텀을 위함 (+ react-hook-form)
+ * @returns
+ */
+
 const Input = ({
   width,
   fontSize,
@@ -8,6 +30,7 @@ const Input = ({
   label,
   placeholder,
   errorMessage,
+  ...props
 }: InputProps) => {
   return (
     <InputWrapper
@@ -15,7 +38,10 @@ const Input = ({
       $fontSize={fontSize}
     >
       <Label $margin={margin}>{label}</Label>
-      <StyledInput placeholder={placeholder} />
+      <StyledInput
+        placeholder={placeholder}
+        {...props}
+      />
       <ErrorMessage $margin={margin}>{errorMessage}</ErrorMessage>
     </InputWrapper>
   );
