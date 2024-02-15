0d5d53c947f912cbf2dced068630dddabe2d991a
SoJuSo <asdfg7123@gmail.com>
2024-02-15 14:27:27 +0900

    💄design : 이미지용 스타일링 코드 추가 및 기타 수정
    날개 이미지를 담기 위한 스타일링 코드를 생성하였습니다.
    내비게이션 바 태그를 div -> nav로 변경하였습니다.

--- src/components/common/Navigator/WebNavBar/WebNavBar.style.tsx
+++ src/components/common/Navigator/WebNavBar/WebNavBar.style.tsx
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 import { Row } from '@/styles/globalStyles';
 
-export const WebNavBarWrapper = styled(Row)`
+export const WebNavBarWrapper = styled.nav`
+  display: flex;
   max-width: 1140px;
   margin: 0 auto;
   padding: 0.5rem 3rem;
@@ -21,12 +22,22 @@ export const WebNavBarMyPage = styled(Row)`
   gap: 2rem;
 `;
 
-export const WebNavBarLogo = styled.div`
+export const WebNavBarLogo = styled(Row)`
   cursor: pointer;
   font-size: 3.5rem;
   font-weight: bold;
   white-space: nowrap;
-  color: #009c4d; // todo. theme props 형태로 변경
+  color: #48da79; // todo. theme props 형태로 변경
+`;
+
+export const WebNavBarLogoImage = styled.div`
+  cursor: pointer;
+  width: 4rem;
+  height: 4rem;
+  background-image: url('/kiwing_logos_transparent_fit_square.png');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
 `;
 
 export const WebNavItem = styled.div<{ $isLocated: boolean }>`
