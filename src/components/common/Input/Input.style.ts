e98d162dfd06f225cd682874aa777bbfd0c2c2f4
JaeHyunGround <gothddlek@naver.com>
2024-02-15 18:13:47 +0900

    ✨feat : Input 컴포넌트에 쓰일 스타일 정의
    Input 컴포넌트에 쓰일 스타일을 정의했습니다.
    
    - InputWrapper, Label, StyledInput, ErrorMessage 이름을 가지는 styled-components를 정의했습니다.

---
+++ src/components/common/Input/Input.style.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+import { InputStyledProps } from './Input.type';
+
+export const InputWrapper = styled.div<InputStyledProps>`
+  width: ${(props) => props.$width};
+  font-size: ${(props) => props.$fontSize}rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Label = styled.label<InputStyledProps>`
+  color: ${({ theme }) => theme.primary_color};
+  margin-bottom: ${(props) => (props.$margin ? props.$margin : 1.5)}rem;
+`;
+
+export const StyledInput = styled.input`
+  color: ${({ theme }) => theme.primary_color};
+  background-color: transparent;
+  padding: 0 0.4rem 0.2rem 0.4rem;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 0.15rem solid ${({ theme }) => theme.primary_color};
+  outline: none;
+  transition: border 0.3s;
+
+  &:focus {
+    border-bottom: 0.15rem solid ${({ theme }) => theme.symbol_secondary_color};
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => theme.gray_300};
+  }
+`;
+
+export const ErrorMessage = styled.div<InputStyledProps>`
+  color: ${({ theme }) => theme.error_red};
+  margin-top: ${(props) => (props.$margin ? props.$margin + 0.15 : 1.65)}rem;
+`;
