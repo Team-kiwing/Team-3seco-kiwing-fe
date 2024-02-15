2998c939ae128a44adc6eb7926561d068b7227e3
kim-hyunjoo <khj9x.x@gmail.com>
2024-02-16 01:41:03 +0900

    💡comment : Button 컴포넌트 간단한 주석 추가

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -4,6 +4,11 @@ import { useTheme } from 'styled-components';
 import StyledButton from './Button.style';
 import { ButtonProps } from './Button.type';
 
+/**
+ * @brief 범용적으로 사용할 수 있는 사각형 버튼입니다.
+ * @param width px,rem,% 단위 커스텀 가능
+ * @param height px,rem,% 단위 커스텀 가능
+ */
 const Button = ({
   width = '10rem',
   height = '3rem',
