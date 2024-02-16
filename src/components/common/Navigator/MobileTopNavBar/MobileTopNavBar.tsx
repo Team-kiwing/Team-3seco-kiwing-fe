b61ae7c3fdbcc5e999c63f04c72cb4a481a38b38
SoJuSo <asdfg7123@gmail.com>
2024-02-17 01:08:23 +0900

    ✨feat : 모바일 상단 내비게이션 바 기능 추가
    - 뒤로가기 버튼을 추가, 구현하였습니다.
    - 라이트, 다크모드 버튼을 구현하였습니다.
    - 아바타 이미지 출력, 클릭시 마이페이지로의 이동을 구현하였습니다.

--- src/components/common/Navigator/MobileTopNavBar/MobileTopNavBar.tsx
+++ src/components/common/Navigator/MobileTopNavBar/MobileTopNavBar.tsx
@@ -1,3 +1,54 @@
-const MobileTopNavBar = () => {};
+import { MdArrowBack, MdDarkMode, MdLightMode } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+
+import { themeStore } from '@/stores';
+
+import Avatar from '../../Avatar';
+import IconWrapper from '../../IconWrapper';
+import { TopBarItem, TopNavBarWrapper } from './MobileTopNavBar.style';
+
+const MobileTopNavBar = () => {
+  const { isDarkMode, updateTheme } = themeStore();
+  const navigator = useNavigate();
+
+  return (
+    <TopNavBarWrapper $isDark={isDarkMode}>
+      <TopBarItem>
+        <IconWrapper
+          onClick={() => {
+            navigator(-1);
+          }}
+          $size={'s'}
+        >
+          <MdArrowBack />
+        </IconWrapper>
+      </TopBarItem>
+      <TopBarItem>
+        {isDarkMode ? (
+          <IconWrapper
+            onClick={updateTheme}
+            $size={'xs'}
+          >
+            <MdLightMode />
+          </IconWrapper>
+        ) : (
+          <IconWrapper
+            onClick={updateTheme}
+            $size={'xs'}
+          >
+            <MdDarkMode />
+          </IconWrapper>
+        )}
+        <Avatar
+          onClick={() => navigator('/@:id')}
+          style={{ transform: 'scale(0.9)' }}
+          $size="nav"
+          // todo 동적으로 로그인 한 경우에만 이미지 주소를 주도록
+          $src="https://avatars.githubusercontent.com/u/90549862?v=4"
+        />
+      </TopBarItem>
+    </TopNavBarWrapper>
+  );
+};
 
 export default MobileTopNavBar;
