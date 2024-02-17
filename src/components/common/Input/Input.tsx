9058c7f20d4361b28cc3cc7754323b08c72c1ad7
JaeHyunGround <gothddlek@naver.com>
2024-02-17 17:44:45 +0900

    ♻️refactor : Input 컴포넌트 jsDoc 수정
    jsDoc 오타를 수정했습니다.

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -18,7 +18,7 @@ import { InputProps } from './Input.type';
  * @param margin : 선택 | number 타입. 라벨, input, 에러 메시지 간 간격을 의미. 생략 시 기본 값으로 1.5가 설정. (단위는 rem)
  * @param label : 선택 | string 타입. input 위에 붙는 라벨 역할.
  * @param placeholder : 선택 | string 타입. input의 placeholder 역할.
- * @param errorMessage : 선택 | string 타입. input의 placeholder 역할.
+ * @param errorMessage : 선택 | string 타입. input의 에러 메시지 역할.
  * @param ...props : 커스텀을 위함 (+ react-hook-form)
  * @returns
  */
