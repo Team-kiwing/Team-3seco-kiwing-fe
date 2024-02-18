28c4b96f404546b06f6f95631e48c4d5a0b13015
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-02-18 21:17:44 +0900

    [#29] Input 컴포넌트 구현 v1.0.0 (#45)
    * ✨feat : Input 컴포넌트 기본 파일 세팅
    
    Input 컴포넌트에 필요한 기본 파일들을 세팅했습니다.
    
    * ✨feat : 에러 메시지에 쓰일 빨간색 색깔코드를 전역 테마에 추가
    
    에러 메시지에 쓰일 빨간색 색깔코드를 전역 테마에 추가했습니다.
    
    * ✨feat : Input 컴포넌트에 쓰일 타입 선언
    
    Input 컴포넌트에 쓰일 타입들을 선언했습니다.
    
    * ✨feat : Input 컴포넌트에 쓰일 스타일 정의
    
    Input 컴포넌트에 쓰일 스타일을 정의했습니다.
    
    - InputWrapper, Label, StyledInput, ErrorMessage 이름을 가지는 styled-components를 정의했습니다.
    
    * ✨feat : Input 컴포넌트 구현
    
    Input 컴포넌트를 구현했습니다.
    
    - InputWrapper 자식으로 Label, StyledInput, ErrorMessage가 배치됩니다.
    
    * 🐛fix : InputProps에 fontSize props를 선택 사항으로 변경
    
    InputProps에 fontSize props를 선택 사항으로 변경했습니다.
    
    * 🐛fix : font-size 기본값 설정
    
    선택 사항인 fontSize를 대응하기 위해 InputWrapper의 font-size에 삼항 연산자를 사용하여 기본값을 설정했습니다.
    
    * ✨feat : Input 컴포넌트 jsDoc 작성
    
    Input 컴포넌트에 jsDoc를 작성했습니다.
    
    * 🐛fix : errorMessage props 유무에 따라 ErrorMessage 컴포넌트 조건부 렌더링
    
    errorMessage props 유무에 따라 ErrorMessage 컴포넌트를 조건부 렌더링되게 수정했습니다.
    
    - errorMessage props가 넘어오지 않았을 때 ErrorMessage 컴포넌트 영역이 잡히는 것을 방지합니다.
    
    * ♻️refactor : border-bottom transition delay 수정
    
    Input 컴포넌트의 border-bottom transition delay를 0.3 => 0.2로 수정했습니다.
    
    * ♻️refactor : InputWrapper에 Col 스타일 적용
    
    InputWrapper에 Col 스타일을 적용했습니다.
    
    * ♻️refactor : errorMessage 영역 문제 해결
    
    errorMessage의 유무에 따라 UI가 달라지는 문제를 해결했습니다.
    
    * ♻️refactor : Input 컴포넌트 jsDoc 수정
    
    jsDoc 오타를 수정했습니다.
    
    * ♻️refactor : font-size 기본값 수정
    
    Input 컴포넌트의 폰트 사이즈 기본값을 1.6rem으로 변경했습니다.
    
    * ♻️refactor : 에러 메시지 폰트 사이즈 수정
    
    에러 메시지의 폰트 사이즈를 라벨과 인풋 폰트 사이즈와 차이를 주기 위해 -0.6rem의 차이를 주었습니다.
    
    - fontSize props가 넘어오지 않을 경우 기본 값은 1rem 입니다.
    
    * ♻️refactor : Input 컴포넌트 jsDoc 수정
    
    에러 메시지 폰트 사이즈 관련 jsDoc를 수정했습니다.

---
+++ src/components/common/Input/Input.style.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+import { Col } from '@/styles/globalStyles';
+
+import { InputStyledProps } from './Input.type';
+
+export const InputWrapper = styled(Col)<InputStyledProps>`
+  width: ${(props) => props.$width};
+  font-size: ${(props) => (props.$fontSize ? props.$fontSize : 1.6)}rem;
+`;
+
+export const Label = styled.label<InputStyledProps>`
+  color: ${({ theme }) => theme.primary_color};
+  margin-bottom: ${(props) => (props.$margin ? props.$margin : 1.5)}rem;
+`;
+
+export const StyledInput = styled.input`
+  color: ${({ theme }) => theme.primary_color};
+  background-color: transparent;
+  padding: 0 0.4rem 0.2rem 0.4rem;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 0.15rem solid ${({ theme }) => theme.primary_color};
+  outline: none;
+  transition: border 0.2s;
+
+  &:focus {
+    border-bottom: 0.15rem solid ${({ theme }) => theme.symbol_secondary_color};
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => theme.gray_300};
+  }
+`;
+
+export const ErrorMessage = styled.div<InputStyledProps>`
+  font-size: ${(props) => (props.$fontSize ? props.$fontSize - 0.6 : 1)}rem;
+  color: ${({ theme }) => theme.error_red};
+  margin-top: ${(props) => (props.$margin ? props.$margin + 0.15 : 1.65)}rem;
+  visibility: ${({ $isError }) => ($isError ? 'hidden' : undefined)};
+`;
