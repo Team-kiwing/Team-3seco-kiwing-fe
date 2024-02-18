b796c2e39782dd8a84b7a2c9d54013711b049528
Kyungbin <pkb8839@naver.com>
2024-02-18 18:53:39 +0900

    ✨feat : Selector 컴포넌트 구현

--- src/components/common/Selector/Selector.tsx
+++ src/components/common/Selector/Selector.tsx
@@ -1,5 +1,46 @@
-const Selector = () => {
-  <></>;
+import { useState } from 'react';
+
+import {
+  FirstContent,
+  SecondContent,
+  SelectStick,
+  SelectWrapper,
+} from './Selector.style';
+import { SelectorProps } from './Selector.type';
+
+const Selector = ({
+  content,
+  size = 1.7,
+  onSelected,
+  ...props
+}: SelectorProps) => {
+  const [isTrue, setIsTrue] = useState(true);
+
+  const handleSelected = (item: boolean) => {
+    setIsTrue(item);
+    onSelected(item);
+  };
+  return (
+    <>
+      <SelectWrapper {...props}>
+        <FirstContent
+          $size={size}
+          isSelected={isTrue}
+          onClick={() => handleSelected(true)}
+        >
+          {content[0]}
+        </FirstContent>
+        <SelectStick $size={size} />
+        <SecondContent
+          $size={size}
+          isSelected={isTrue}
+          onClick={() => handleSelected(false)}
+        >
+          {content[1]}
+        </SecondContent>
+      </SelectWrapper>
+    </>
+  );
 };
 
 export default Selector;
