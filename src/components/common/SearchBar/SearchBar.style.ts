68fb07efb6262e2ba50b537547026151994a7d03
JaeHyunGround <gothddlek@naver.com>
2024-02-19 02:42:32 +0900

    ✨feat : SearchBar 컴포넌트 스타일 구현
    SearchBar 컴포넌트의 Wrapper 스타일을 제작했습니다.

---
+++ src/components/common/SearchBar/SearchBar.style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+import { MOBILE } from '@/constants';
+
+export const SearchBarWrapper = styled.div`
+  @media screen and (max-width: ${MOBILE}px) {
+    margin: 0 auto;
+  }
+
+  position: relative;
+  width: 80%;
+  max-width: 432px;
+
+  & > :nth-child(2) {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+`;
