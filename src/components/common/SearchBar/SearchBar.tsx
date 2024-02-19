87f1772dba753ce99ee89a0951ba82c9f0d01d79
JaeHyunGround <gothddlek@naver.com>
2024-02-19 15:54:59 +0900

    ✨feat : SearchBar 컴포넌트 구현
    SearchBar 컴포넌트를 구현했습니다.

--- src/components/common/SearchBar/SearchBar.tsx
+++ src/components/common/SearchBar/SearchBar.tsx
@@ -1,5 +1,34 @@
-const SearchBar = () => {
-  return <div>검색바 컴포넌트</div>;
+import { MdSearch } from 'react-icons/md';
+
+import IconWrapper from '../IconWrapper';
+import Input from '../Input';
+import { SearchBarWrapper } from './SearchBar.style';
+import { SearchBarProps } from './SearchBar.type';
+
+const SearchBar = ({
+  handleSearchIcon,
+  handleSubmit,
+  ...props
+}: SearchBarProps) => {
+  return (
+    <SearchBarWrapper>
+      <form onSubmit={handleSubmit}>
+        <Input
+          width={'100%'}
+          margin={0}
+          placeholder={'검색어를 입력해주세요.'}
+          style={{ paddingRight: '3rem' }}
+          {...props}
+        />
+      </form>
+      <IconWrapper
+        $size={'xs'}
+        onClick={handleSearchIcon}
+      >
+        <MdSearch />
+      </IconWrapper>
+    </SearchBarWrapper>
+  );
 };
 
 export default SearchBar;
