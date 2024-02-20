52ea558a23b90a6310d670e21b991799fada0d50
SoJuSo <asdfg7123@gmail.com>
2024-02-21 00:25:37 +0900

    ✨feat : NotFound 페이지 구현
    NotFound 페이지를 구현하였습니다.
    홈으로 버튼과 더불어 혹시모를 오류 사항을 대비하여 오류 페이지로도
    리다이렉트 할 수 있는 버튼을 추가하였습니다.

--- src/pages/NotFoundPage/NotFound.tsx
+++ src/pages/NotFoundPage/NotFound.tsx
@@ -1,7 +1,74 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useTheme } from 'styled-components';
+
+import Button from '@/components/common/Button';
+import EasterEgg from '@/components/common/EasterEgg';
+import ShadowBox from '@/components/common/ShadowBox';
+
+import {
+  EasterEggText,
+  NotFoundButtonWrap,
+  NotFoundLayout,
+  NotFoundLogoImage,
+  NotFoundText,
+  PTagText,
+} from './NotFound.style';
+
 const NotFound = () => {
+  const navigator = useNavigate();
+  const theme = useTheme();
+  const [showEasterEgg, setShowEasterEgg] = useState(false);
+
+  const handleClickHome = () => {
+    navigator('/');
+  };
+
+  const handleClickError = () => {
+    navigator('/report');
+  };
+
+  const handleLogoClick = () => {
+    setShowEasterEgg(true);
+    setTimeout(() => {
+      setShowEasterEgg(false);
+    }, 7000);
+  };
+
   return (
     <>
-      <div>NotFoundPage 입니다.</div>
+      <NotFoundLayout>
+        <EasterEggText $isShow={showEasterEgg}>
+          <ShadowBox
+            style={{ flexDirection: 'column', padding: '0 2rem' }}
+            width="auto"
+            height="auto"
+          >
+            <PTagText>키윙 이스터 에그를 찾으셨네요!</PTagText>
+            <PTagText>여러분의 멋진 삶을 응원합니다.</PTagText>
+          </ShadowBox>
+        </EasterEggText>
+
+        {showEasterEgg && <EasterEgg />}
+        <NotFoundLogoImage
+          src="/kiwing_square_green.png"
+          alt="키윙 로고"
+          onClick={handleLogoClick}
+        />
+        <NotFoundText>요청하신 페이지를 찾지 못했어요</NotFoundText>
+        <NotFoundButtonWrap>
+          <Button
+            onClick={handleClickHome}
+            text="홈으로"
+          />
+          <Button
+            onClick={handleClickError}
+            text="오류 신고하기"
+            backgroundColor={theme.error_red}
+            hoverBackgroundColor={theme.hotBadge_color}
+          />
+        </NotFoundButtonWrap>
+      </NotFoundLayout>
     </>
   );
 };
