62a3ee749661050fd66dc59d6ec1dfafd344f07c
JaeHyunGround <gothddlek@naver.com>
2024-02-21 22:56:03 +0900

    ♻️refactor : UserInfoCard 컴포넌트 jsDoc 수정

--- src/components/common/UserInfoCard/UserInfoCard.tsx
+++ src/components/common/UserInfoCard/UserInfoCard.tsx
@@ -25,8 +25,21 @@ import { UserInfoCardProps } from './UserInfoCard.type';
  *
  * @description UserInfoCard 컴포넌트
  * @summary 사용법 :
- * <UserInfoCard />
- * 데이터 형식이 어떻게 될 지 몰라서, API 명세서가 나온 후 props 작업 진행 할 예정입니다 !
+ * <UserInfoCard
+      userName="JaehyunGround"
+      tags={['프론트엔드', '백엔드', '네트워크']}
+      links={[
+        'https://github.com/Team-kiwing',
+        'https://velog.io/@jaehyun_ground/posts',
+        'https://github.com/Team-kiwing',
+      ]}
+    />
+ * @summary 데이터 형식이 어떻게 될 지 몰라서, 임시로 구현했고 API 명세서가 나온 후 props 수정 작업 진행하겠습니다 !
+
+ * @param userImage : 선택 | string 타입. 유저의 프로필 사진 src를 받습니다.
+ * @param userName : 필수 | string 타입. 유저의 닉네임을 받습니다.
+ * @param tags : 선택 | string[] 타입. 유저가 선택한 태그들을 배열 형태로 받습니다.
+ * @param links : 선택 | string[] 타입. 유저가 등록한 링크들을 배열 형태로 받습니다.
  * @returns
  */
 
