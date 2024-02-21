3c08ddb478b61a398fa65c2ed875f01337d40a21
Kyungbin <pkb8839@naver.com>
2024-02-21 19:31:13 +0900

    💄design : BundleCard 스타일링 변경
    max, min-width 값을 주어 피그마와 동일하게 만들었습니다.

--- src/components/common/BundleCard/Bundle.style.ts
+++ src/components/common/BundleCard/Bundle.style.ts
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 import { FONT_SEMI_BOLD } from '@/constants';
-import { MOBILE_MIN } from '@/constants';
+import { MOBILE, MOBILE_MIN } from '@/constants';
 import { Col } from '@/styles/globalStyles';
 
 export const BundleCardWrapper = styled.div`
   width: 100%;
+  max-width: 40rem;
+  min-width: 26rem;
 `;
 
 export const BundleCardItemWrapper = styled(Col)`
@@ -17,8 +19,15 @@ export const BundleCardItemWrapper = styled(Col)`
 
 export const BundleCardItemName = styled.div`
   font-size: 2rem;
-  font-weight: ${FONT_SEMI_BOLD - 100};
-  margin-bottom: 1.5rem;
+  font-weight: ${FONT_SEMI_BOLD - 150};
+  margin-top: 0.2rem;
+  margin-bottom: 1rem;
+
+  @media screen and (max-width: ${MOBILE}px) {
+    font-size: 1.6rem;
+    margin-bottom: 0.5rem;
+  }
+
   @media screen and (max-width: ${MOBILE_MIN}px) {
     font-size: 1.4rem;
     margin-bottom: 0.5rem;
