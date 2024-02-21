68c666ea5533a53344f1a89475e68b61cd3e802e
JaeHyunGround <gothddlek@naver.com>
2024-02-21 20:14:15 +0900

    ✨feat : 유저 인포 컴포넌트 기본 레이아웃 작업
    유저 인포 컴포넌트의 기본 레이아웃 작업을 진행했습니다.

---
+++ src/components/common/UserInfoCard/UserInfoCard.style.ts
@@ -0,0 +1,89 @@
+import styled from 'styled-components';
+
+import { FONT_REGULAR, MOBILE } from '@/constants';
+import { Col, Row } from '@/styles/globalStyles';
+
+export const UserInfoWrapper = styled(Row)`
+  @media screen and (max-width: ${MOBILE}px) {
+    padding: 1rem;
+  }
+
+  background-color: ${({ theme }) => theme.background_color};
+  width: 100%;
+  padding: 2rem;
+  box-sizing: border-box;
+  align-items: center;
+
+  & > :nth-child(1),
+  & > :nth-child(3) {
+    flex-shrink: 0;
+  }
+
+  & > :nth-child(2) {
+    flex-grow: 1;
+  }
+`;
+
+export const UserInfoContentsWrapper = styled(Col)`
+  @media screen and (max-width: ${MOBILE}px) {
+    padding: 0 1rem;
+  }
+
+  padding: 0 2rem;
+`;
+
+export const UserInfoNicknameBadgeWrapper = styled(Row)`
+  @media screen and (max-width: ${MOBILE}px) {
+    gap: 0.8rem;
+  }
+
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 1rem;
+`;
+
+export const UserInfoNickname = styled.div`
+  @media screen and (max-width: ${MOBILE}px) {
+    font-size: 1.6rem;
+  }
+
+  color: ${({ theme }) => theme.primary_color};
+  font-size: 2rem;
+  font-weight: ${FONT_REGULAR};
+`;
+
+export const UserInfoBadgeWrapper = styled.div`
+  & > span {
+    margin: 0.1rem;
+  }
+`;
+
+export const UserInfoLinkWrapper = styled(Col)`
+  margin-top: 1rem;
+  gap: 0.5rem;
+`;
+
+export const UserInfoLink = styled(Row)`
+  @media screen and (max-width: ${MOBILE}px) {
+    font-size: 0.8rem;
+  }
+
+  color: ${({ theme }) => theme.primary_color};
+  font-size: 1.2rem;
+  gap: 0.3rem;
+  align-items: center;
+  & > a {
+    width: 80%;
+    color: ${({ theme }) => theme.gray_500};
+    text-decoration: none;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
+
+export const UserInfoIconWrapper = styled(Col)`
+  height: 100%;
+  justify-content: space-between;
+  color: ${({ theme }) => theme.primary_color};
+`;
--- src/components/common/UserInfoCard/UserInfoCard.tsx
+++ src/components/common/UserInfoCard/UserInfoCard.tsx
@@ -1,5 +1,119 @@
+import { useEffect, useState } from 'react';
+import { IoIosLink } from 'react-icons/io';
+import { PiNotePencil, PiSignOut } from 'react-icons/pi';
+
+import { MOBILE } from '@/constants';
+
+import Avatar from '../Avatar';
+import Badge from '../Badge';
+import IconWrapper from '../IconWrapper';
+import ShadowBox from '../ShadowBox';
+import {
+  UserInfoBadgeWrapper,
+  UserInfoContentsWrapper,
+  UserInfoIconWrapper,
+  UserInfoLink,
+  UserInfoLinkWrapper,
+  UserInfoNickname,
+  UserInfoNicknameBadgeWrapper,
+  UserInfoWrapper,
+} from './UserInfoCard.style';
+
 const UserInfoCard = () => {
-  return <div>유저 인포 카드</div>;
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+  let timer = null;
+
+  useEffect(() => {
+    const resizeViewportWidth = () => {
+      clearTimeout(timer!);
+      timer = setTimeout(() => {
+        setViewportWidth(window.innerWidth);
+      }, 200);
+    };
+
+    window.addEventListener('resize', resizeViewportWidth);
+
+    return () => {
+      window.removeEventListener('resize', resizeViewportWidth);
+    };
+  }, []);
+
+  return (
+    <ShadowBox
+      width={viewportWidth <= MOBILE ? '90%' : '80%'}
+      height={'100%'}
+      style={{ minWidth: '280px', margin: '0 auto' }}
+    >
+      <UserInfoWrapper>
+        <Avatar $size={viewportWidth <= MOBILE ? 'mobile' : 'pc'} />
+        <UserInfoContentsWrapper>
+          <UserInfoNicknameBadgeWrapper>
+            <UserInfoNickname>닉네임</UserInfoNickname>
+            <UserInfoBadgeWrapper>
+              <Badge
+                $state={'basic'}
+                $size={viewportWidth <= MOBILE ? 'xxs' : 'xs'}
+                $text={'프론트엔드'}
+                style={{ backgroundColor: '#009c4d' }}
+              />
+              <Badge
+                $state={'basic'}
+                $size={viewportWidth <= MOBILE ? 'xxs' : 'xs'}
+                $text={'프론트엔드'}
+                style={{ backgroundColor: '#009c4d' }}
+              />
+              <Badge
+                $state={'basic'}
+                $size={viewportWidth <= MOBILE ? 'xxs' : 'xs'}
+                $text={'프론트엔드'}
+                style={{ backgroundColor: '#009c4d' }}
+              />
+            </UserInfoBadgeWrapper>
+          </UserInfoNicknameBadgeWrapper>
+          <UserInfoLinkWrapper>
+            <UserInfoLink>
+              <IconWrapper $size={viewportWidth <= MOBILE ? 'xss' : 'xs'}>
+                <IoIosLink />
+              </IconWrapper>
+              <a href="https://github.com/Team-kiwing">
+                https://github.com/Team-kiwing
+              </a>
+            </UserInfoLink>
+            <UserInfoLink>
+              <IconWrapper $size={viewportWidth <= MOBILE ? 'xss' : 'xs'}>
+                <IoIosLink />
+              </IconWrapper>
+              <a href="https://velog.io/@jaehyun_ground/posts">
+                https://velog.io/@jaehyun_ground/posts
+              </a>
+            </UserInfoLink>
+            <UserInfoLink>
+              <IconWrapper $size={viewportWidth <= MOBILE ? 'xss' : 'xs'}>
+                <IoIosLink />
+              </IconWrapper>
+              <a href="https://github.com/Team-kiwing">
+                https://github.com/Team-kiwing
+              </a>
+            </UserInfoLink>
+          </UserInfoLinkWrapper>
+        </UserInfoContentsWrapper>
+        <UserInfoIconWrapper>
+          <IconWrapper
+            $size={viewportWidth <= MOBILE ? 'xs' : 's'}
+            onClick={() => alert('회원 정보 수정 모달 띄우는 작업 해야함 !')}
+          >
+            <PiNotePencil />
+          </IconWrapper>
+          <IconWrapper
+            $size={viewportWidth <= MOBILE ? 'xs' : 's'}
+            onClick={() => alert('로그아웃 작업 해야함 !')}
+          >
+            <PiSignOut />
+          </IconWrapper>
+        </UserInfoIconWrapper>
+      </UserInfoWrapper>
+    </ShadowBox>
+  );
 };
 
 export default UserInfoCard;
