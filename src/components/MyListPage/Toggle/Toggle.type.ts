1966e2c56c68ec15a7e7b87a6ab4e5219ded69f4
kim-hyunjoo <khj9x.x@gmail.com>
2024-02-24 03:50:37 +0900

    🗑️remove : 중복 파일 삭제

--- src/components/MyListPage/Toggle/Toggle.type.ts
+++
@@ -1,8 +0,0 @@
-export interface ToggleProps
-  extends React.HtmlHTMLAttributes<HTMLLabelElement> {
-  name?: string;
-  on?: boolean;
-  disabled?: boolean;
-  isColorReverse?: boolean;
-  onChange: () => void;
-}
