ef846dc711f16ae07959def99c24762860129f02
SeokJu Kim <asdfg7123@gmail.com>
2024-02-24 15:28:11 +0900

    [#80] 프로젝트 세팅 v1.1.2 (#81)
    * ✨feat : 정책 페이지 추가하였습니다.
    
    * ✨feat : 정책 페이지 라우터 추가
    
    * 💄design : 전역 테마에 테두리 색상을 정의하였습니다.
    
    라이트 모드 #D9D9D9
    다크 모드 #5E5E5E
    
    * 🎨style : 불필요한 코드 공백 제거

---
+++ src/pages/PolicyPage/Policy.tsx
@@ -0,0 +1,10 @@
+const Policy = () => {
+  // 요기다가 하시면 됩니다
+  return (
+    <>
+      <div>Policy Page 입니다.</div>
+    </>
+  );
+};
+
+export default Policy;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,6 +3,7 @@ export { default as HubPage } from './HubPage';
 export { default as MainPage } from './MainPage';
 export { default as MyListPage } from './MyListPage';
 export { default as NotFoundPage } from './NotFoundPage';
+export { default as PolicyPage } from './PolicyPage';
 export { default as RegisterPage } from './RegisterPage';
 export { default as ReportPage } from './ReportPage';
 export { default as SharedItemPage } from './SharedItemPage';
