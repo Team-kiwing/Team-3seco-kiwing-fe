02d492171d0193c35c8955f915ab14f5595fd476
kim-hyunjoo <khj9x.x@gmail.com>
2024-02-26 01:24:12 +0900

    ✨feat : 드롭다운 각 option마다 핸들러 함수 정의

--- src/components/MyBundlePage/MyBundleDropDown/MyBundleDropDown.tsx
+++ src/components/MyBundlePage/MyBundleDropDown/MyBundleDropDown.tsx
@@ -4,6 +4,9 @@ import { RiDeleteBin5Line, RiFileCopyLine } from 'react-icons/ri';
 import DropDown from '@/components/common/DropDown';
 import IconWrapper from '@/components/common/IconWrapper';
 import Toggle from '@/components/common/Toggle';
+import { PATH } from '@/constants/router';
+
+import { MyBundleDropDownProps } from './MyBundleDropDown.type';
 
 const MyBundleDropDown = ({
   isDropDownShow,
@@ -11,13 +14,29 @@ const MyBundleDropDown = ({
   isShared,
   setIsShared,
   triggerId,
-}: {
-  isDropDownShow: boolean;
-  setIsDropDownShow: (state: boolean) => void;
-  isShared: boolean;
-  setIsShared: (state: boolean) => void;
-  triggerId: string;
-}) => {
+  bundleId,
+}: MyBundleDropDownProps) => {
+  const handleCopyClipBoard = async (text: string) => {
+    try {
+      const SERVICE_URL = window.location.host;
+      await navigator.clipboard.writeText(`${SERVICE_URL}${text}`);
+      // @TODO 추후에 alert -> 토스트 알림으로 변경합니다.
+      alert('클립보드에 링크가 복사되었습니다.');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDeleteBundle = (bundleId: number) => {
+    console.log(`id ${bundleId} 꾸러미 삭제`);
+    // @TODO 추후에 꾸러미 삭제 API 함수를 호출합니다.
+  };
+
+  const handleEditBundle = (bundleId: number) => {
+    console.log(`id ${bundleId} 꾸러미 편집`);
+    // @TODO 추후에 꾸러미 편집을 할 수 있는 모달을 띄웁니다.
+  };
+
   const options = [
     {
       id: 1,
@@ -25,8 +44,8 @@ const MyBundleDropDown = ({
       rightItem: <Toggle on={isShared} />,
 
       handler: () => {
-        console.log('공개여부 클릭');
         setIsShared(!isShared);
+        // @TODO 추후에 꾸러미 공개/비공개를 결정하는 api 로직을 연동합니다.
       },
     },
 
@@ -39,7 +58,12 @@ const MyBundleDropDown = ({
         </IconWrapper>
       ),
       handler: () => {
-        console.log('링크 복사');
+        if (isShared) {
+          handleCopyClipBoard(`${PATH.SHARED}/${bundleId}`);
+        } else {
+          // @TODO 추후에 alert -> 토스트 알림으로 변경합니다.
+          alert('공개된 리스트만 링크를 복사할 수 있습니다.');
+        }
         setIsDropDownShow(false);
       },
     },
@@ -52,7 +76,7 @@ const MyBundleDropDown = ({
         </IconWrapper>
       ),
       handler: () => {
-        console.log('리스트 삭제');
+        handleDeleteBundle(bundleId);
         setIsDropDownShow(false);
       },
     },
@@ -65,7 +89,7 @@ const MyBundleDropDown = ({
         </IconWrapper>
       ),
       handler: () => {
-        console.log('리스트 편집');
+        handleEditBundle(bundleId);
         setIsDropDownShow(false);
       },
     },
