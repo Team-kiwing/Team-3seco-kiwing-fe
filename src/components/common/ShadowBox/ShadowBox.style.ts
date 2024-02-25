bfec76ffc19cf104f002a58b1c2b49b66ddacdc8
kim-hyunjoo <khj9x.x@gmail.com>
2024-02-25 23:23:43 +0900

    💄design : 그림자 조절 0.05 -> 0.1로 변경

--- src/components/common/ShadowBox/ShadowBox.style.ts
+++ src/components/common/ShadowBox/ShadowBox.style.ts
@@ -11,7 +11,7 @@ export const Container = styled.div<ContainerProps>`
   background-color: ${(props) => props.$color};
   box-shadow: 0.2rem 0.2rem 2rem 0.5rem
     ${(props) =>
-      props.$isDarkMode ? 'rgba(255, 255, 255, 0.05)' : 'rgba(0, 0, 0, 0.05)'};
+      props.$isDarkMode ? 'rgba(255, 255, 255, 0.1)' : 'rgba(0, 0, 0, 0.1)'};
   border-radius: 1rem;
   transition: all 0.2s ease;
 
