db7541735afe19212eb62301c69b282757f811f2
SoJuSo <asdfg7123@gmail.com>
2024-02-26 00:42:25 +0900

    ✨feat : 네비게이터에서 사용되는 공용 훅 정의
    Navigator에서 사용되는 로그인 여부 탐지, 라우팅 기능을 수행합니다.

---
+++ src/components/common/Navigator/Navigator.hook.ts
@@ -0,0 +1,45 @@
+// 이 부분 로직이 WebNavBar.hook이랑 겹쳐서 상위 Navigator 폴더 훅으로 마이그레이션 예정입니다.
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { PATH } from '@/constants/router';
+
+export const useNavigatorMenu = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const location = useLocation();
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    // Todo 로그인 여부
+    // const id = getId();
+    // if (id) setIsLogin(true);
+    setIsLogin(false);
+    // setIsLogin(true);
+  }, []);
+
+  const handleNavigate = (path: string): void => {
+    if (location.pathname === path) {
+      return;
+    } else {
+      navigator(path);
+    }
+  };
+
+  const handleLogo = () => handleNavigate(PATH.MAIN);
+  const handleHub = () => handleNavigate(PATH.HUB);
+  const handleSharedList = () => handleNavigate(PATH.SHARED_ITEM);
+  const handleShared = () => handleNavigate(PATH.SHARED);
+  const handleLogin = () => handleNavigate(PATH.AUTH);
+  const handleMyList = () => handleNavigate(PATH.MY);
+
+  return {
+    isLogin,
+    location,
+    handleLogo,
+    handleHub,
+    handleShared,
+    handleSharedList,
+    handleMyList,
+    handleLogin,
+  };
+};
