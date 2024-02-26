8444541f364d7ade701661c0e1caece1972a1152
Kyungbin <pkb8839@naver.com>
2024-02-26 12:49:18 +0900

    [#63] Badge 컴포넌트 구현 v1.2.0 (#82)
    * ✨feat : 전역테마 색깔 추가
    
    * ✨feat : subscribed 뱃지 추가
    
    - subscribed 뱃지 스타일 추가하였습니다.
    - subscribedCount의 타입을 number타입으로 지정했습니다.
    
    * ✨feat : scribedTag 뱃지 추가
    
    scribedTag 태그를 만들었습니다.
    - $state에 scribedTag 추가했습니다.
    - 그에 따른 스타일을 추가했습니다.
    - 1000이하는 숫자만 붙이고 그 이후부터는 K+가 붙습니다.
    - jsDoc을 다시 작성하였습니다.
    
    * 🎨style : 불필요한 코드 삭제
    
    필요없는 @media 코드를 삭제하였습니다.
    
    * ✨feat : 전역 다크모드 테마에 색상 추가
    
    스크랩 뱃지에 쓰이는 색상을 전역테마 다크모드에 추가합니다.
    
    * ♻️refactor : PR반영하여 코드 리펙토링
    
    - 해쉬태그스타일에 배경색이 필요없으므로 투명색을 주었습니다.
    - useMemo를 사용하여 가독성을 올리고 최적화하였습니다.
    
    * 💄design : css 스타일링 수정
    
    - 북마크 아이콘을 다른것으로 변경하였습니다.
    - 아이콘 사이즈도 변경하였습니다.
    - Hot, Scribed 뱃지는 cursor: default를 주었습니다.
    
    * 🎨style : useMemo 삭제
    
    - useMeme 코드를 삭제하였습니다.

--- src/components/common/Badge/Badge.tsx
+++ src/components/common/Badge/Badge.tsx
@@ -1,6 +1,8 @@
+import { BsBookmarksFill } from 'react-icons/bs';
+
+import IconWrapper from '../IconWrapper/IconWrapper';
 import { StyledBadge } from './Badge.style';
 import { PropsBadge } from './Badge.type';
-
 /**
  * @summary 사용법 <Badge
       $size={'m'}
@@ -9,12 +11,17 @@ import { PropsBadge } from './Badge.type';
       $isHover={true}
       $text={'백엔드'}
     />
+        <Badge
+          $size={'m'}
+          $state="subscribedTag"
+          $subscribedCount={11150}
+        />
  * @description 공통 Badge 컴포넌트
- * @param $size font-size의 크기
- * @param $state basic, focus, hashTag, hot이 있으면 background-color와 color의 색깔을 지정합니다.
- * @param $margin margin 값을 받습니다.
- * @param $hover true false에 따라 hover가 적용됩니다.
- * @param $text 뱃지안에 들어간 text값 입니다.
+ * @param $size 필수) font-size의 크기입니다 'xxs, xs, s, m, l' 타입과 number타입이 있습니다.
+ * @param $state 필수) basic, focus, hashTag, hot이 있으면 background-color와 color의 색깔을 지정합니다.
+ * @param $margin 선택) margin 값을 받습니다.
+ * @param $hover 선택) true false에 따라 hover가 적용됩니다.
+ * @param $text 선택) 뱃지안에 들어가는 값입니다. (ReactNode타입 입니다.)
  * @returns
  */
 
@@ -24,18 +31,44 @@ const Badge = ({
   $size,
   $text,
   $margin,
+  $subscribedCount,
   ...props
 }: PropsBadge) => {
+  const displayText = () => {
+    if ($subscribedCount !== undefined && $state === 'subscribedTag') {
+      return (
+        <>
+          <IconWrapper
+            style={{ cursor: 'default' }}
+            $size={$size === 'xs' ? 1.5 : 1.8}
+          >
+            <BsBookmarksFill />
+          </IconWrapper>
+          <span>
+            {$subscribedCount < 1000
+              ? $subscribedCount
+              : `${Math.floor($subscribedCount / 100) / 10}K+`}
+          </span>
+        </>
+      );
+    } else if ($state === 'hashTag') {
+      return `#${$text}`;
+    }
+    return $text;
+  };
+
   return (
-    <StyledBadge
-      $size={$size}
-      $state={$state}
-      $isHover={$isHover}
-      $margin={$margin}
-      {...props}
-    >
-      {$state === 'hashTag' ? `#${$text}` : $text}
-    </StyledBadge>
+    <>
+      <StyledBadge
+        $size={$size}
+        $state={$state}
+        $isHover={$isHover}
+        $margin={$margin}
+        {...props}
+      >
+        {displayText()}
+      </StyledBadge>
+    </>
   );
 };
 
