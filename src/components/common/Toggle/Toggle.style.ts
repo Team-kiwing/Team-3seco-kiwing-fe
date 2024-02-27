8e0e2703f2dd657c712e15dd89394aa5576f2bd2
hyunjoo <khj9x.x@gmail.com>
2024-02-27 23:52:13 +0900

    💄design : Toggle border 두께 0.2rem으로 변경

--- src/components/common/Toggle/Toggle.style.ts
+++ src/components/common/Toggle/Toggle.style.ts
@@ -18,7 +18,7 @@ export const ToggleSwitch = styled.div<ToggleSwitchProps>`
   width: 100%;
   height: 100%;
 
-  border: 0.1rem solid
+  border: 0.2rem solid
     ${(props) =>
       props.$isBorderShow ? props.theme.border_color : 'transparent'};
 
