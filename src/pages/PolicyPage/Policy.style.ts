9a548fb47afd213ef6279469e3717c6fceb0cc03
JaeHyunGround <gothddlek@naver.com>
2024-02-28 00:00:20 +0900

    ♻️refactor : 필요 없는 코드 제거

--- src/pages/PolicyPage/Policy.style.ts
+++ src/pages/PolicyPage/Policy.style.ts
@@ -9,25 +9,6 @@ import {
 } from '@/constants';
 import { Col } from '@/styles/globalStyles';
 
-export const Header = styled.div`
-  display: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  background-color: red;
-  align-items: center;
-  justify-content: space-between;
-  margin: 0 auto;
-  padding: 3rem 2rem 0.5rem 2rem;
-  max-width: 1140px;
-  font-size: 2rem;
-  font-weight: 500;
-  user-select: none;
-  @media screen and (max-width: ${MOBILE}px) {
-    display: none;
-  }
-`;
-
 export const PolicyPageWrapper = styled(Col)`
   width: 70%;
   margin: 0 auto;
