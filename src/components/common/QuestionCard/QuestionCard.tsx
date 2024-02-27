25073ee1436ad966c4117753447b71f9b81d69ed
SoJuSo <asdfg7123@gmail.com>
2024-02-28 00:55:46 +0900

    ✨feat : QuestionCard 기본 레이아웃, 데이터 입력 정의
    props로 내려받은 데이터를 처리하는 로직을 추가하였습니다.
    style을 적용하였습니다.

--- src/components/common/QuestionCard/QuestionCard.tsx
+++ src/components/common/QuestionCard/QuestionCard.tsx
@@ -1,3 +1,89 @@
-const QuestionCard = () => {};
+import useResize from '@/hooks/useResize';
+
+import Badge from '../Badge';
+import CircleButton from '../CircleButton';
+import ShadowBox from '../ShadowBox';
+import {
+  QuestionCardAddButton,
+  QuestionCardBadgeWrapper,
+  QuestionCardBodyWrapper,
+  QuestionCardContainer,
+  QuestionCardHashTags,
+  QuestionCardInfoBadges,
+  QuestionCardReportBadge,
+  QuestionCardText,
+} from './QuestionCard.style';
+import { QuestionCardProps } from './QuestionCard.type';
+
+const QuestionCard = ({
+  id,
+  question,
+  hashTags,
+  subscribedCount,
+  isHot,
+}: QuestionCardProps) => {
+  const { isMobileSize } = useResize();
+  return (
+    <>
+      <ShadowBox
+        style={{
+          position: 'relative',
+          maxWidth: '1140px',
+          boxSizing: 'border-box',
+          cursor: 'auto',
+        }}
+        isCard={true}
+        width="100%"
+        height="fit-content"
+      >
+        <QuestionCardAddButton>
+          <CircleButton />
+        </QuestionCardAddButton>
+
+        <QuestionCardContainer id={String(id)}>
+          <QuestionCardBodyWrapper>
+            <QuestionCardText>{question}</QuestionCardText>
+            <QuestionCardHashTags>
+              {hashTags.map((item) => (
+                <Badge
+                  key={item.id}
+                  $size={isMobileSize ? 'xs' : 's'}
+                  $state="hashTag"
+                  $text={item.tagName}
+                />
+              ))}
+            </QuestionCardHashTags>
+          </QuestionCardBodyWrapper>
+
+          <QuestionCardBadgeWrapper>
+            <QuestionCardInfoBadges>
+              {isHot && (
+                <Badge
+                  style={{ cursor: 'default' }}
+                  $size={isMobileSize ? 'xs' : 's'}
+                  $state="hot"
+                  $text="HOT"
+                />
+              )}
+              <Badge
+                $size={isMobileSize ? 'xs' : 's'}
+                $state="subscribedTag"
+                $subscribedCount={subscribedCount}
+              />
+            </QuestionCardInfoBadges>
+            <QuestionCardReportBadge>
+              <Badge
+                $isHover
+                $size={'xs'}
+                $state="basic"
+                $text="신고"
+              />
+            </QuestionCardReportBadge>
+          </QuestionCardBadgeWrapper>
+        </QuestionCardContainer>
+      </ShadowBox>
+    </>
+  );
+};
 
 export default QuestionCard;
