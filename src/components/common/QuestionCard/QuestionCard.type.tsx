50b1b7deee85237f43688a72fcf4148f6e020324
SoJuSo <asdfg7123@gmail.com>
2024-02-28 00:54:20 +0900

    ✨feat : QuestionCard 타입 정의

--- src/components/common/QuestionCard/QuestionCard.type.tsx
+++ src/components/common/QuestionCard/QuestionCard.type.tsx
@@ -1,1 +1,11 @@
-// type
+interface tagProps {
+  id: number;
+  tagName: string;
+}
+export interface QuestionCardProps {
+  id: number;
+  question: string;
+  subscribedCount: number;
+  isHot: boolean;
+  hashTags: tagProps[];
+}
