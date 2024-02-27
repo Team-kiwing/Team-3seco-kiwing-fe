7b7cd6173638f8ea3c76bf5e95eaaccd36d28853
JaeHyunGround <gothddlek@naver.com>
2024-02-27 23:13:50 +0900

    ✨feat : Policy 페이지 스타일 생성
    Policy 페이지에 필요한 스타일을 선언했습니다.

---
+++ src/pages/PolicyPage/Policy.style.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+import {
+  FONT_REGULAR,
+  FONT_SEMI_BOLD,
+  MOBILE,
+  MOBILE_FONT_SIZE,
+  WEB_FONT_SIZE,
+} from '@/constants';
+import { Col } from '@/styles/globalStyles';
+
+export const PolicyPageWrapper = styled(Col)`
+  width: 70%;
+  margin: 0 auto;
+  gap: 3rem;
+  padding: 3rem 0;
+
+  @media screen and (max-width: ${MOBILE}px) {
+    padding: 7.5rem 0;
+    gap: 2rem;
+  }
+`;
+
+export const PolicyTitle = styled.div`
+  color: ${({ theme }) => theme.primary_color};
+  font-size: ${WEB_FONT_SIZE + 1}rem;
+  font-weight: ${FONT_SEMI_BOLD};
+`;
+
+export const PolicyContent = styled.div`
+  color: ${({ theme }) => theme.primary_color};
+  font-size: ${WEB_FONT_SIZE}rem;
+  font-weight: ${FONT_REGULAR};
+  white-space: pre-wrap;
+  line-height: 2.5rem;
+
+  @media screen and (max-width: ${MOBILE}px) {
+    font-size: ${MOBILE_FONT_SIZE}rem;
+    line-height: 2rem;
+  }
+`;
