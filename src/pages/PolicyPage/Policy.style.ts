579d7ed46239e5d3b0dc1d87405e3217e1c64166
JaeHyunGround <gothddlek@naver.com>
2024-02-27 23:42:01 +0900

    💄design : 웹 상단 nav를 고려하여 padding 값 추가
    웹 상단 nav를 고려하여 PolicyPageWrapper에 padding값을 추가했습니다.

--- src/pages/PolicyPage/Policy.style.ts
+++ src/pages/PolicyPage/Policy.style.ts
@@ -9,14 +9,32 @@ import {
 } from '@/constants';
 import { Col } from '@/styles/globalStyles';
 
+export const Header = styled.div`
+  display: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: red;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 3rem 2rem 0.5rem 2rem;
+  max-width: 1140px;
+  font-size: 2rem;
+  font-weight: 500;
+  user-select: none;
+  @media screen and (max-width: ${MOBILE}px) {
+    display: none;
+  }
+`;
+
 export const PolicyPageWrapper = styled(Col)`
   width: 70%;
   margin: 0 auto;
   gap: 3rem;
-  padding: 3rem 0;
+  padding: 7.5rem 0;
 
   @media screen and (max-width: ${MOBILE}px) {
-    padding: 7.5rem 0;
     gap: 2rem;
   }
 `;
