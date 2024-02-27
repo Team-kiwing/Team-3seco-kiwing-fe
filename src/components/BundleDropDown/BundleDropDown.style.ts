16e82711945cdc9d9a87856ede8d4a42c57acdbf
Kyungbin <pkb8839@naver.com>
2024-02-27 10:50:03 +0900

    🚚rename : 폴더 구조 변경 및 폴더이름 변경

--- src/components/BundleDropDown/BundleDropDown.style.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const BundleDropDownWrapper = styled.div<{ $width: string }>`
-  position: absolute;
-  bottom: 0;
-  right: ${({ $width }) => $width};
-`;
