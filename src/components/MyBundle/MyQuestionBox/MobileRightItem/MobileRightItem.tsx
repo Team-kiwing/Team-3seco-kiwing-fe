b0c6791398f4a4f97b3fc753aebcde5c477d5efc
hyunjoo <khj9x.x@gmail.com>
2024-02-28 00:19:25 +0900

    🎨style : 추후에 생길 DropDown 가이드 라인 생성

--- src/components/MyBundle/MyQuestionBox/MobileRightItem/MobileRightItem.tsx
+++ src/components/MyBundle/MyQuestionBox/MobileRightItem/MobileRightItem.tsx
@@ -3,10 +3,17 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 import IconWrapper from '@/components/common/IconWrapper';
 
 const MobileRightItem = () => {
+  const handleClick = () => {
+    // @TODO DropDown컴포넌트 만든 후 열리는 로직 추가
+    console.log('드롭다운');
+  };
+
   return (
     <IconWrapper
+      id="my-question-dropdown-btn"
       $size={'s'}
       $isBackground={true}
+      onClick={handleClick}
     >
       <RxHamburgerMenu />
     </IconWrapper>
