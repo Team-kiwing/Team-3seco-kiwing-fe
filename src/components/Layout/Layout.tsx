14214e1c3221e841fd2efd217f645fda62db9dcc
SoJuSo <asdfg7123@gmail.com>
2024-02-28 15:19:45 +0900

    💄design : 내비게이션 바에 따른 전역 레이아웃 코드 추가

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import { styled } from 'styled-components';
+
+import { MOBILE } from '@/constants';
+
+const Layout = styled.div<{ $isMobile: boolean }>`
+  width: 100%;
+  height: 100%;
+  padding-top: 1rem;
+
+  @media screen and (max-width: ${MOBILE}px) {
+    padding-top: 5rem;
+    padding-bottom: 6rem;
+  }
+`;
+
+export default Layout;
