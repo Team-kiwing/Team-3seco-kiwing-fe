b3977a05cc22d075a6f1e436e41936065409ca5d
kim-hyunjoo <khj9x.x@gmail.com>
2024-02-28 18:23:35 +0900

    💄design : 어색한 css 개선

---
+++ src/components/MyBundle/MyBundleList/MyBundleList.style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div<{ $isMobileSize: boolean }>`
+  width: ${({ $isMobileSize }) => ($isMobileSize ? '100%' : '20%')};
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: auto;
+  gap: 1.5rem;
+`;
+
+export const BundleWrapper = styled.div`
+  width: 85%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1.5rem;
+`;
--- src/components/MyBundle/MyBundleList/MyBundleList.tsx
+++ src/components/MyBundle/MyBundleList/MyBundleList.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import { useTheme } from 'styled-components';
 
 import Button from '@/components/common/Button';
+import { FONT_MEDIUM } from '@/constants';
 import useResize from '@/hooks/useResize';
 
 import MyBundleItem from '../MyBundleItem';
+import { BundleWrapper, Container } from './MyBundleList.style';
 
 export interface Bundle {
   id: number;
@@ -17,36 +19,33 @@ const MyBundleList = ({ bundles }: { bundles: Bundle[] }) => {
   const theme = useTheme();
   const [selectedBundle, setSelectedBundle] = useState<Bundle | null>(null);
 
-  return bundles.map((bundle, index) => (
-    <div
-      key={index}
-      style={{
-        width: '85%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: '1.5rem',
-      }}
-    >
-      <MyBundleItem
-        selectedBundle={selectedBundle}
-        setSelectedBundle={setSelectedBundle}
-        bundle={bundle}
-        isMobileSize={isMobileSize}
-      />
-      <Button
-        width="85%"
-        height="5rem"
-        borderColor={`${theme.symbol_color}`}
-        hoverBackgroundColor={`${theme.symbol_color}`}
-        backgroundColor="transparent"
-        textColor={`${theme.symbol_color}`}
-        text="+ 새 질문 꾸러미 추가하기"
-        style={{ flexShrink: 0 }}
-      />
-    </div>
-  ));
+  return (
+    <Container $isMobileSize={isMobileSize}>
+      <BundleWrapper>
+        {bundles.map((bundle) => (
+          <MyBundleItem
+            key={bundle.id}
+            selectedBundle={selectedBundle}
+            setSelectedBundle={setSelectedBundle}
+            bundle={bundle}
+            isMobileSize={isMobileSize}
+          />
+        ))}
+        <Button
+          width="100%"
+          height="5rem"
+          borderColor={`${theme.symbol_color}`}
+          hoverBackgroundColor={`${theme.symbol_color}`}
+          backgroundColor="transparent"
+          textColor={`${theme.symbol_color}`}
+          text="+ 새 질문 꾸러미 추가하기"
+          style={{
+            fontWeight: `${FONT_MEDIUM}`,
+          }}
+        />
+      </BundleWrapper>
+    </Container>
+  );
 };
 
 export default MyBundleList;
