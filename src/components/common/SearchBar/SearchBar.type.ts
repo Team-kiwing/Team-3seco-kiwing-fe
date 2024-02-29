d103ec5cb75db8538b04f09a828b6d84784df983
JaeHyunGround <gothddlek@naver.com>
2024-02-29 17:56:37 +0900

    🐛fix : react-hook-form 적용을 위한 REGISTER, VALIDATE 타입 추가 및 submit 함수는 1개로 통일
    - react-hook-form 적용을 위해 REGISTER, VALIDATE 타입을 추가했습니다.
    - submit 함수는 1개로 통일 했습니다.

--- src/components/common/SearchBar/SearchBar.type.ts
+++ src/components/common/SearchBar/SearchBar.type.ts
@@ -1,7 +1,10 @@
+import { RegisterOptions } from 'react-hook-form';
+
 export interface SearchBarProps extends React.HTMLAttributes<HTMLInputElement> {
-  handleSearchIcon?: () => void;
-  handleFormSubmit?: () => void;
+  handleSearchSubmit: () => void;
   maxWidth?: string;
+  REGISTER: string;
+  VALIDATE?: RegisterOptions;
 }
 
 export interface SearchBarStyleProps {
