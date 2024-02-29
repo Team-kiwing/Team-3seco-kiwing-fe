9d2e07fabe483860a5da332f6530de587f321b73
Kyungbin <pkb8839@naver.com>
2024-03-01 03:27:51 +0900

    ♻️refactor : PR반영하여 코드 수정
    PR반영하여 SharedPage에서 쓰이는 파일들을 분리하였습니다.

--- src/pages/SharedPage/Shared.tsx
+++ src/pages/SharedPage/Shared.tsx
@@ -1,146 +1,89 @@
 import { useEffect, useState } from 'react';
-import styled from 'styled-components';
 
 import BundleCard from '@/components/common/BundleCard';
-import { WebNavBar } from '@/components/common/Navigator';
 import SearchBar from '@/components/common/SearchBar';
 import Selector from '@/components/common/Selector';
 import TagFilter from '@/components/common/TagFilter';
-import { TagProps } from '@/components/common/TagFilter/TagFilter.type';
 
-const SharedWrapper = styled.div`
-  width: 70%;
-  display: flex;
-  flex-direction: column;
-  margin: 0 auto;
-  gap: 3rem;
-  @media screen and (max-width: 768px) {
-    width: 90%;
-  }
-`;
-
-const TopWrapper = styled.div`
-  width: 100%;
-  margin: 5rem auto;
-  display: flex;
-  justify-content: center;
-`;
-
-const SearchWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  margin: 0 auto;
-  gap: 2rem;
-  justify-content: start;
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-  }
-`;
-
-const SelectorWrapper = styled.div`
-  display: flex;
-  justify-content: end;
-`;
-
-const CardWrapper = styled.div`
-  width: 100%;
-  display: grid;
-  margin: 0 auto;
-  grid-row-gap: 2rem;
-  grid-column-gap: 10rem;
-  grid-template-columns: repeat(2, 1fr);
-  box-sizing: border-box;
-  justify-items: center;
-  @media screen and (max-width: 1000px) {
-    grid-template-columns: repeat(1, 1fr);
-  }
-`;
-
-interface Tag {
-  id: number;
-  tagName: string;
-}
-
-interface InterviewList {
-  id: number;
-  name: string;
-  shareType: string;
-  scrapeCount: number;
-  tags: Tag[];
-  isHot: boolean;
-  createdAt: string;
-  updatedAt: string;
-}
+import { SelectorConstants } from './Shared.const';
+import {
+  CardWrapper,
+  SearchWrapper,
+  SelectorWrapper,
+  SharedWrapper,
+  TagFilterWrapper,
+} from './Shared.style';
+import { BundleProps, TagProps } from './Shared.type';
 
 const Shared = () => {
   const [selectedTags, setSelectedTags] = useState<TagProps[]>([]);
   const [isRecent, setIsRecent] = useState<boolean>(true);
-  const [list, setList] = useState<InterviewList[]>([]);
+  const [bundles, setBundles] = useState<BundleProps[]>([]);
 
   useEffect(() => {
     fetch('/bundles')
       .then((res) => res.json())
-      .then((data) => setList(data));
+      .then((data) => setBundles(data));
   }, []);
-  if (list.length === 0) {
+  if (bundles.length === 0) {
     return null;
   }
 
   const tagList = [
     // 나중에 api 로직으로 대체
-    { id: 1, name: '개발자' },
-    { id: 2, name: '백엔드' },
-    { id: 3, name: '서버' },
-    { id: 4, name: '시스템' },
-    { id: 5, name: '보안' },
-    { id: 6, name: '프론트엔드' },
-    { id: 7, name: '머신러닝' },
-    { id: 8, name: 'QA' },
-    { id: 9, name: '안드로이드' },
-    { id: 10, name: '크로스플랫폼앱' },
-    { id: 11, name: '데이터' },
-    { id: 12, name: 'SW' },
-    { id: 13, name: 'DevOps' },
-    { id: 14, name: 'DBA' },
-    { id: 15, name: '네트워크' },
-    { id: 16, name: '웹퍼블리셔' },
-    { id: 17, name: '임베디드' },
-    { id: 18, name: 'IOS' },
-    { id: 19, name: 'HW' },
+    { id: 1, content: '개발자' },
+    { id: 2, content: '백엔드' },
+    { id: 3, content: '서버' },
+    { id: 4, content: '시스템' },
+    { id: 5, content: '보안' },
+    { id: 6, content: '프론트엔드' },
+    { id: 7, content: '머신러닝' },
+    { id: 8, content: 'QA' },
+    { id: 9, content: '안드로이드' },
+    { id: 10, content: '크로스플랫폼앱' },
+    { id: 11, content: '데이터' },
+    { id: 12, content: 'SW' },
+    { id: 13, content: 'DevOps' },
+    { id: 14, content: 'DBA' },
+    { id: 15, content: '네트워크' },
+    { id: 16, content: '웹퍼블리셔' },
+    { id: 17, content: '임베디드' },
+    { id: 18, content: 'IOS' },
+    { id: 19, content: 'HW' },
   ];
 
   return (
     <>
-      <WebNavBar />
       <SharedWrapper>
-        <TopWrapper>
+        <TagFilterWrapper>
           <TagFilter
             selectedTags={selectedTags}
             setSelectedTags={setSelectedTags}
             tagList={tagList}
           />
-        </TopWrapper>
-
+        </TagFilterWrapper>
         <SearchWrapper>
           <SearchBar />
         </SearchWrapper>
         <SelectorWrapper>
           <Selector
             isState={isRecent}
-            content={['최근순', '스크랩 순']}
-            onSelected={(item) => setIsRecent(item)}
+            content={[
+              SelectorConstants.RECENT_SORT_TEXT,
+              SelectorConstants.POPULAR_SORT_TEXT,
+            ]}
+            onSelected={setIsRecent}
           />
         </SelectorWrapper>
-
         <CardWrapper>
-          {list.map((item) => (
+          {bundles.map((bundle) => (
             <BundleCard
-              key={item.id}
-              id={item.id}
-              bundleName={item.name}
-              hashTags={item.tags}
-              isHot={item.isHot}
-              subscribedCount={item.scrapeCount}
+              key={bundle.id}
+              id={bundle.id}
+              bundleName={bundle.name}
+              hashTags={bundle.tags}
+              isHot={bundle.isHot}
+              subscribedCount={bundle.scrapeCount}
             />
           ))}
         </CardWrapper>
