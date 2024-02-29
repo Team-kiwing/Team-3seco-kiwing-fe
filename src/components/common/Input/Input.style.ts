bf12e18f219df377ca4c1f39ea7f7cfa6fe22b40
JaeHyunGround <gothddlek@naver.com>
2024-02-29 18:57:08 +0900

    💄design : isOnlyBorderBottom props에 따른 스타일 컨트롤

--- src/components/common/Input/Input.style.ts
+++ src/components/common/Input/Input.style.ts
@@ -1,9 +1,29 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { MOBILE } from '@/constants';
 import { Col } from '@/styles/globalStyles';
 
 import { InputStyledProps } from './Input.type';
+
+const borderStyle = {
+  onlyBottom: css`
+    border-bottom: 0.15rem solid ${({ theme }) => theme.primary_color};
+    border-radius: 0;
+
+    &:focus {
+      border-bottom: 0.15rem solid
+        ${({ theme }) => theme.symbol_secondary_color};
+    }
+  `,
+  all: css`
+    border: 0.15rem solid ${({ theme }) => theme.primary_color};
+    border-radius: 10rem;
+
+    &:focus {
+      border: 0.15rem solid ${({ theme }) => theme.symbol_secondary_color};
+    }
+  `,
+};
 
 export const InputWrapper = styled(Col)<InputStyledProps>`
   @media screen and (max-width: ${MOBILE}px) {
@@ -19,19 +39,17 @@ export const Label = styled.label<InputStyledProps>`
   margin-bottom: ${(props) => (props.$margin ? props.$margin : 1.5)}rem;
 `;
 
-export const StyledInput = styled.input`
+export const StyledInput = styled.input<InputStyledProps>`
   color: ${({ theme }) => theme.primary_color};
   background-color: transparent;
-  padding: 0 0.4rem 0.2rem 0.4rem;
+  padding: 0.2rem 0.4rem 0.2rem 0.4rem;
   box-sizing: border-box;
   border: none;
-  border-bottom: 0.15rem solid ${({ theme }) => theme.primary_color};
   outline: none;
   transition: border 0.2s;
 
-  &:focus {
-    border-bottom: 0.15rem solid ${({ theme }) => theme.symbol_secondary_color};
-  }
+  ${({ $isOnlyBorderBottom }) =>
+    $isOnlyBorderBottom ? borderStyle['onlyBottom'] : borderStyle['all']}
 
   &::placeholder {
     color: ${({ theme }) => theme.gray_300};
