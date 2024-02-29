08855ce1bdd3d5b18b0d06bc27a45390ed963f07
Kyungbin <pkb8839@naver.com>
2024-03-01 05:23:13 +0900

    [#155] Selector 컴포넌트 v1.2.0 (#156)
    * ♻️refactor : 정의된 타입의 네이밍 변경
    
    * 🐛fix : 토글되는 버그 수정
    
    - 토글되는 버그를 수정하였습니다.
    - 같은 걸 클릭해도 이벤트가 실행되는것을 막았습니다.
    - 상수화 된 값으로 스타일 파일을 다시 작성하였습니다.

--- src/components/common/Selector/Selector.tsx
+++ src/components/common/Selector/Selector.tsx
@@ -15,36 +15,39 @@ import { SelectorProps } from './Selector.type';
  * @description 공통 Selector 컴포넌트
  * @param content 필수) Selector 들어갈 text를 의미합니다. string[] 형태입니다. ex) ['전체', '내가 작성한 글']
  * @param isState 필수) boolean 타입의 true, false 상태값 입니다.
+ * @param setIsState 필수) 선택이 변경될 때 호출되는 콜백 함수입니다. 선택된 항목의 상태를 부모 컴포넌트에게 전달합니다.
  * @param size 선택) 글씨 크기입니다. (rem 단위이고 number타입입니다.)
- * @param onSelected 필수) 선택이 변경될 때 호출되는 콜백 함수입니다. 선택된 항목의 상태를 부모 컴포넌트에게 전달합니다.
  * @returns
  */
 
 const Selector = ({
   content,
   size = 1.7,
-  onSelected,
+  setIsState,
   isState,
   ...props
 }: SelectorProps) => {
   const handleSelected = (item: boolean) => {
-    onSelected(item);
+    if (item == !isState) {
+      setIsState(item);
+      console.log(item);
+    }
   };
   return (
     <>
       <SelectWrapper {...props}>
         <FirstContent
           $size={size}
-          $isSelected={isState}
-          onClick={() => handleSelected(!isState)}
+          $setIsState={isState}
+          onClick={() => handleSelected(true)}
         >
           {content[0]}
         </FirstContent>
         <SelectStick $size={size} />
         <SecondContent
           $size={size}
-          $isSelected={isState}
-          onClick={() => handleSelected(!isState)}
+          $setIsState={isState}
+          onClick={() => handleSelected(false)}
         >
           {content[1]}
         </SecondContent>
