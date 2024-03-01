dee3c8f41cba9991cfafa4352d87f058873c8974
Kyungbin <pkb8839@naver.com>
2024-03-01 16:36:32 +0900

    ✨feat : tags API 연동 및 Selector 코드 수정
    실제 tags의 API연동을 진행하였습니다.
    Selector의 버전이 바뀌면서 코드 또한 수정하였습니다.

--- src/pages/SharedPage/Shared.tsx
+++ src/pages/SharedPage/Shared.tsx
@@ -19,38 +19,21 @@ const Shared = () => {
   const [selectedTags, setSelectedTags] = useState<TagProps[]>([]);
   const [isRecent, setIsRecent] = useState<boolean>(true);
   const [bundles, setBundles] = useState<BundleProps[]>([]);
+  const [tags, setTags] = useState<TagProps[]>([]);
 
   useEffect(() => {
     fetch('/bundles')
       .then((res) => res.json())
       .then((data) => setBundles(data));
+
+    fetch('/api/v1/tags')
+      .then((res) => res.json())
+      .then((res) => setTags(res.data));
   }, []);
+
   if (bundles.length === 0) {
     return null;
   }
-
-  const tagList = [
-    // 나중에 api 로직으로 대체
-    { id: 1, content: '개발자' },
-    { id: 2, content: '백엔드' },
-    { id: 3, content: '서버' },
-    { id: 4, content: '시스템' },
-    { id: 5, content: '보안' },
-    { id: 6, content: '프론트엔드' },
-    { id: 7, content: '머신러닝' },
-    { id: 8, content: 'QA' },
-    { id: 9, content: '안드로이드' },
-    { id: 10, content: '크로스플랫폼앱' },
-    { id: 11, content: '데이터' },
-    { id: 12, content: 'SW' },
-    { id: 13, content: 'DevOps' },
-    { id: 14, content: 'DBA' },
-    { id: 15, content: '네트워크' },
-    { id: 16, content: '웹퍼블리셔' },
-    { id: 17, content: '임베디드' },
-    { id: 18, content: 'IOS' },
-    { id: 19, content: 'HW' },
-  ];
 
   return (
     <>
@@ -59,7 +42,7 @@ const Shared = () => {
           <TagFilter
             selectedTags={selectedTags}
             setSelectedTags={setSelectedTags}
-            tagList={tagList}
+            tagList={tags}
           />
         </TagFilterWrapper>
         <SearchWrapper>
@@ -68,11 +51,11 @@ const Shared = () => {
         <SelectorWrapper>
           <Selector
             isState={isRecent}
+            setIsState={setIsRecent}
             content={[
               SelectorConstants.RECENT_SORT_TEXT,
               SelectorConstants.POPULAR_SORT_TEXT,
             ]}
-            onSelected={setIsRecent}
           />
         </SelectorWrapper>
         <CardWrapper>
