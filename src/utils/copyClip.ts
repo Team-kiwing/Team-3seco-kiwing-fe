26e7dafe09edbcd54d5779d2c64c3657be4f65a6
kim-hyunjoo <khj9x.x@gmail.com>
2024-03-04 19:52:23 +0900

    💄design : 링크 복사 성공 시 alert -> notify로 변경

--- src/utils/copyClip.ts
+++ src/utils/copyClip.ts
@@ -1,7 +1,10 @@
+import { notify } from '@/hooks/toast';
+
+const SUCCESS_NOTIFY = '클립보드에 링크가 복사되었습니다.';
 export const handleCopyClipBoard = async (host: string, pathname: string) => {
   try {
     await navigator.clipboard.writeText(`${host}${pathname}`);
-    alert('클립보드에 링크가 복사되었습니다.');
+    notify({ type: 'success', text: SUCCESS_NOTIFY });
   } catch (err) {
     console.log(err);
   }
