99cf9650bd0f5699f7a0c839a6c7ef6551507930
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-03-05 01:35:36 +0900

    [#122] AuthPage v1.0.0 (#180)
    * ✨feat : AuthPage 제작을 위한 기본 파일 생성
    
    * ✨feat : 구글 로그인 이미지 추가
    
    * ✨feat : AuthPage props 세팅
    
    $isMobile, $isDarkMode props를 추가했습니다.
    
    * 💄design : AuthPage 레이아웃을 위한 기본 스타일 생성
    
    AuthPage의 기본 레이아웃 작업을 위해 기본 스타일을 생성했습니다.
    
    * ✨feat : AuthPage 기본 레이아웃 작업
    
    * ♻️refactor : 서비스 소개글 수정
    
    * ♻️refactor : PR 리뷰 반영
    
    - header에 h1 태그 적용
    - 구조 분해 할당으로 props 사용
    - 로고의 최대값 부여
    
    * ♻️refactor : 구글 로그인 url 상수화
    
    * 💄design : $isMobile props 제거, 미디어 쿼리 적용

---
+++ src/pages/AuthPage/Auth.tsx
@@ -0,0 +1,73 @@
+import { useTheme } from 'styled-components';
+
+import useResize from '@/hooks/useResize';
+import { themeStore } from '@/stores';
+
+import { SignInUrl } from './Auth.const';
+import {
+  AuthContentWrapper,
+  AuthDescription,
+  AuthGoogleWrapper,
+  AuthHeader,
+  AuthLogo,
+  AuthPageWrapper,
+  AuthSubHeader,
+} from './Auth.style';
+
+const Auth = () => {
+  const { isMobileSize } = useResize();
+  const { isDarkMode } = themeStore();
+  const theme = useTheme();
+
+  const goGoogleLogin = () => {
+    window.location.href = SignInUrl;
+  };
+
+  return (
+    <AuthPageWrapper>
+      {!isMobileSize && (
+        <AuthLogo>
+          <img
+            src="./kiwing_circle_green.png"
+            alt="kiwing logo"
+          />
+        </AuthLogo>
+      )}
+      <AuthContentWrapper>
+        <AuthHeader>
+          <span
+            style={{
+              color: isDarkMode
+                ? `${theme.symbol_secondary_color}`
+                : `${theme.symbol_color}`,
+            }}
+          >
+            kiwing
+          </span>
+          에 오신 것을 환영합니다.
+        </AuthHeader>
+        <AuthDescription>
+          kiwing은 취업 준비생, 이직생들이 마주한 면접과정을 준비하는 데에
+          도움이 되고자 기획된 장기 프로젝트입니다. <br /> kiwing은 사용자들에게
+          면접을 효율적이고 빠르게 준비할 수 있는 기능들을 제공합니다.
+        </AuthDescription>
+        <AuthSubHeader>이메일로 로그인 해보세요.</AuthSubHeader>
+        <AuthGoogleWrapper
+          $isDarkMode={isDarkMode}
+          onClick={goGoogleLogin}
+        >
+          <img
+            src={
+              isDarkMode
+                ? './google_sign_in_dark.png'
+                : './google_sign_in_light.png'
+            }
+            alt="구글 로그인"
+          />
+        </AuthGoogleWrapper>
+      </AuthContentWrapper>
+    </AuthPageWrapper>
+  );
+};
+
+export default Auth;
