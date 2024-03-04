145f2b38b416c6403134e124041a21221e7c0e86
Kyungbin <pkb8839@naver.com>
2024-03-04 23:32:17 +0900

    ♻️refactor : 2가지 훅으로 수정
    많은 훅들을 최적화하고 줄여서 2가지의 훅으로 만들었습니다.
    1.useLatestBundles 검색필터의 결과를 최신순으로 불러옵니다.
    2.usePopularBundles 검색필터의 결과를 인기순으로 불러옵니다.

--- src/pages/SharedPage/Shared.hook.ts
+++ src/pages/SharedPage/Shared.hook.ts
@@ -3,93 +3,41 @@ import { useQuery } from '@tanstack/react-query';
 import { searchBundles } from '@/services/bundles';
 import { BundleSearchResponse } from '@/types';
 
-export const useLatestBundles = () => {
+export const useLatestBundles = (tagsId: number[], keyword: string) => {
+  const LatestSelectedTagBundlesKey =
+    tagsId.length === 0 ? [] : tagsId.join(', ');
+  console.log(LatestSelectedTagBundlesKey, keyword);
   const query = useQuery<BundleSearchResponse | null>({
-    queryKey: ['latestBundles'],
-    queryFn: () =>
-      searchBundles({
-        sortingType: 'LATEST',
-        tagIds: [],
-        keyword: '',
-        page: 1,
-        size: 10,
-      }),
-  });
-  return query;
-};
-
-export const usePopularBundles = () => {
-  const query = useQuery<BundleSearchResponse | null>({
-    queryKey: ['popularBundles'],
-    queryFn: () =>
-      searchBundles({
-        sortingType: 'POPULAR',
-        tagIds: [],
-        keyword: '',
-        page: 1,
-        size: 10,
-      }),
-  });
-  return query;
-};
-
-export const useLatestSelectedTagBundles = (tagsId: number[]) => {
-  const LatestSelectedTagBundlesKey = tagsId.join(', ');
-  const query = useQuery<BundleSearchResponse | null>({
-    queryKey: [`latestSelectedTag-${LatestSelectedTagBundlesKey}`],
+    queryKey: [
+      `latest-${keyword}-${typeof LatestSelectedTagBundlesKey === 'string' ? LatestSelectedTagBundlesKey : ''}`,
+    ],
     queryFn: () =>
       searchBundles({
         sortingType: 'LATEST',
         tagIds: LatestSelectedTagBundlesKey,
-        keyword: '',
-        page: 1,
-        size: 10,
-      }),
-  });
-  return query;
-};
-
-export const usePopularSelectedTagBundles = (tagsId: number[]) => {
-  const popularSelectedTagBundlesKey = tagsId.join(', ');
-  const query = useQuery<BundleSearchResponse | null>({
-    queryKey: [`popularSelectedTag-${popularSelectedTagBundlesKey}`],
-    queryFn: async () =>
-      searchBundles({
-        sortingType: 'POPULAR',
-        tagIds: popularSelectedTagBundlesKey,
-        keyword: '',
-        page: 1,
-        size: 10,
-      }),
-  });
-  return query;
-};
-
-export const useLatestSearchedBundles = (keyword: string) => {
-  const query = useQuery<BundleSearchResponse | null>({
-    queryKey: [`latest-${keyword}`],
-    queryFn: () =>
-      searchBundles({
-        sortingType: 'LATEST',
-        tagIds: [],
         keyword: keyword,
         page: 1,
-        size: 10,
+        size: 30,
       }),
   });
   return query;
 };
 
-export const usePopularSearchedBundles = (keyword: string) => {
+export const usePopularBundles = (tagsId: number[], keyword: string) => {
+  const LatestSelectedTagBundlesKey =
+    tagsId.length === 0 ? [] : tagsId.join(', ');
+  console.log(LatestSelectedTagBundlesKey, keyword);
   const query = useQuery<BundleSearchResponse | null>({
-    queryKey: [`popular-${keyword}`],
+    queryKey: [
+      `popular-${keyword}-${typeof LatestSelectedTagBundlesKey === 'string' ? LatestSelectedTagBundlesKey : ''}`,
+    ],
     queryFn: () =>
       searchBundles({
         sortingType: 'POPULAR',
-        tagIds: [],
+        tagIds: LatestSelectedTagBundlesKey,
         keyword: keyword,
         page: 1,
-        size: 10,
+        size: 30,
       }),
   });
   return query;
