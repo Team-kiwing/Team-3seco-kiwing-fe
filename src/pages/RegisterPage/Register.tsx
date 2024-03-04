64f16cc2281a5fe44968e30b3a0e89d37615e33b
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-03-04 23:23:29 +0900

    [#121] RegisterPage v1.0.0 (#171)
    * ✨feat : RegisterPage 기본 파일 생성
    
    * ✨feat : 기본 파일 추가
    
    * ✨feat : 스타일에서 환경 구분을 위한 props type 선언
    
    * 💄design : RegisterPage 기본 스타일 생성
    
    기본 레이아웃에 필요한 스타일을 생성했습니다.
    
    * ✨feat : RegisterPage 레이아웃 작업
    
    RegisterPage의 기본 레이아웃 작업을 진행했습니다.
    
    * 💄design : 모바일 환경에선 hover effect가 발생하지 않도록 방어 코드 작성
    
    * ♻️refactor : 가입 버튼을 눌렀을 때 나오는 문구를 alert => toast 로 수정
    
    * 💄design : 다크 모드 or 라이트 모드에 따른 로고 네임이 눈에 띌 수 있도록 색상 설정
    
    * ✨feat : useFetchTags hook을 생성했습니다.
    
    태그 목록을 가져오는 useFetchTags hook을 생성했습니다.
    
    - queryKeys 라는 상수 파일을 만들어 쿼리 키를 관리할 수 있도록 했습니다.
    
    * ✨feat : RegisterPage에서 useFetchTags hook을 통해 필터 목록을 가져와 적용
    
    데이터를 불러오는 중에는 Skeleton을 사용하여 처리했습니다.
    
    * ♻️refactor : 코드 가독성을 위한 코드 배치 수정
    
    * 🗑️remove : 빈 파일 제거
    
    * 💄design : props 구조 분해 할당 사용, 로고 이미지 너비 최대값 부여
    
    * ♻️refactor : 로고 이미지 수정
    
    * ♻️refactor : 필요없는 코드 삭제
    
    * ♻️refactor : 페이지 헤더 띄워쓰기 추가
    
    * ♻️refactor : useFetchTags의 queryFn에 화살표 함수 적용

---
+++ src/pages/RegisterPage/Register.tsx
@@ -0,0 +1,148 @@
+import { useState } from 'react';
+import { useTheme } from 'styled-components';
+
+import Input from '@/components/common/Input';
+import Skeleton from '@/components/common/Skeleton';
+import TagFilter from '@/components/common/TagFilter';
+import { notify } from '@/hooks/toast';
+import { useFetchTags } from '@/hooks/useFetchTags';
+import useResize from '@/hooks/useResize';
+import { themeStore } from '@/stores';
+import { Tag } from '@/types';
+
+import {
+  RegisterCheckbox,
+  RegisterCheckboxWrapper,
+  RegisterFormWrapper,
+  RegisterHeader,
+  RegisterItemWrapper,
+  RegisterLabel,
+  RegisterLogo,
+  RegisterPageWrapper,
+  RegisterSubmitButton,
+} from './Register.style';
+
+const Register = () => {
+  const { isMobileSize } = useResize();
+  const { isDarkMode } = themeStore();
+  const theme = useTheme();
+
+  const { data: tags, isLoading } = useFetchTags();
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
+  const [isChecked, setIsChecked] = useState(false);
+  const handleRegisterSubmit = () => {
+    if (isChecked) {
+      notify({ type: 'success', text: '가입에 성공했습니다 !' });
+    } else {
+      notify({
+        type: 'error',
+        text: '이용약관에 동의해야 서비스에 가입할 수 있습니다.',
+      });
+    }
+  };
+
+  return (
+    <RegisterPageWrapper>
+      {!isMobileSize && (
+        <RegisterLogo>
+          <img
+            src="./kiwing_circle_green.png"
+            alt="kiwing logo"
+          />
+        </RegisterLogo>
+      )}
+      <RegisterFormWrapper $isMobile={isMobileSize}>
+        <RegisterHeader $isMobile={isMobileSize}>
+          안녕하세요. 저희는&nbsp;
+          <span
+            style={{
+              color: isDarkMode
+                ? `${theme.symbol_secondary_color}`
+                : `${theme.symbol_color}`,
+            }}
+          >
+            kiwing
+          </span>
+          이에요.
+        </RegisterHeader>
+        <RegisterItemWrapper>
+          <Input
+            fontSize={1.8}
+            label={'이름'}
+            width="100%"
+            placeholder="Input의 props로 disabled 속성을 추가한 뒤 구글 로그인 한 결과에서 이름을 넣어야함."
+          />
+        </RegisterItemWrapper>
+        <RegisterItemWrapper>
+          <Input
+            fontSize={1.8}
+            label={'이메일'}
+            width="100%"
+            placeholder="Input의 props로 disabled 속성을 추가한 뒤 구글 로그인 한 결과에서 이메일을 넣어야함."
+          />
+        </RegisterItemWrapper>
+        <RegisterItemWrapper>
+          <Input
+            fontSize={1.8}
+            label={'사용자 이름'}
+            width="100%"
+            errorMessage="영어, 숫자 이외에 문자를 넣으면 출력"
+            placeholder="영어와 숫자 조합으로 적어주세요."
+          />
+        </RegisterItemWrapper>
+        <RegisterItemWrapper>
+          <RegisterLabel $isMobile={isMobileSize}>관심 분야</RegisterLabel>
+          {isLoading ? (
+            <Skeleton.Box
+              $width={'100%'}
+              $height={'20rem'}
+            />
+          ) : (
+            <TagFilter
+              selectedTags={selectedTags}
+              setSelectedTags={setSelectedTags}
+              tagList={tags ?? []}
+            />
+          )}
+        </RegisterItemWrapper>
+        <RegisterItemWrapper>
+          <Input
+            fontSize={1.8}
+            label={'링크 추가'}
+            width="100%"
+            placeholder="자신을 뽐낼 수 있는 링크를 추가해보세요."
+          />
+          <Input
+            fontSize={1.8}
+            width="100%"
+            placeholder="자신을 뽐낼 수 있는 링크를 추가해보세요."
+          />
+          <Input
+            fontSize={1.8}
+            width="100%"
+            placeholder="자신을 뽐낼 수 있는 링크를 추가해보세요."
+          />
+        </RegisterItemWrapper>
+        <RegisterCheckboxWrapper $isMobile={isMobileSize}>
+          <RegisterCheckbox
+            type="checkbox"
+            checked={isChecked}
+            onChange={() => setIsChecked((idx) => !idx)}
+          />
+          <div>
+            <a href="/policy">이용약관</a>에 동의합니다.
+          </div>
+        </RegisterCheckboxWrapper>
+        <RegisterSubmitButton
+          onClick={handleRegisterSubmit}
+          $isMobile={isMobileSize}
+        >
+          가입
+        </RegisterSubmitButton>
+      </RegisterFormWrapper>
+    </RegisterPageWrapper>
+  );
+};
+
+export default Register;
