dfb5d1d6457249b7732dbf39ca2a9dc3671ed0ea
kim-hyunjoo <khj9x.x@gmail.com>
2024-03-05 17:08:15 +0900

    💄design : css 디자인 적용

---
+++ src/components/MyBundle/MyQuestionModal/MyQuestionModal.style.ts
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components';
+
+export const ModalContainer = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+`;
