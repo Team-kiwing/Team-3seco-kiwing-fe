325e36a44fe182fe3f72bbf1ce3772e3f6a47b23
Kyungbin <pkb8839@naver.com>
2024-03-05 17:42:28 +0900

    ✨feat : SharedBundleDropDown 컴포넌트 구현
    버전이 바뀌면서 SharedBundleDropDown을 다시 구현하였습니다.
    그에 따른 사용법인 jsDoc을 다시 작성하였습니다.

--- src/components/SharedBundle/SharedBundleDropDown/SharedBundleDropDown.tsx
+++ src/components/SharedBundle/SharedBundleDropDown/SharedBundleDropDown.tsx
@@ -4,37 +4,47 @@ import { useLocation } from 'react-router-dom';
 
 import DropDown from '@/components/common/DropDown';
 import IconWrapper from '@/components/common/IconWrapper';
+import { notify } from '@/hooks/toast';
 import { handleCopyClipBoard } from '@/utils/copyClip';
 
-import { SharedBundleDropDownWrapper } from './SharedBundleDropDown.style';
 import { SharedBundleDropDownProps } from './SharedBundleDropDown.type';
 
 /**
  * @summary 사용법    <SharedBundleDropDown
-          isDropDownShow={isDropDownShow}
-          setIsDropDownShow={setIsDropDownShow}
-          triggerId="dropdown1-btn"
-          width="14rem"
-        />
-        SharedBundleDropDown는 position: absolute 속성이 있습니다.
-        SharedBundleDropDown 사용하는 부모컴포넌트에 무조건 position: 'relative'를 주어야 합니다. 
+          bundleId={1}
+          isShow={isShow}
+          setIsShow={setIsShow}
+          closeDropDown={closeDropDown}
+          direction="bottom-right"
+          onAddBundle={() => console.log('test')}
+        /> 
  * @description SharedBundleDropDown 컴포넌트
- * @param isDropDownShow 필수) SharedBundleDropDown가 보여지는 유무를 나타내는 상태값입니다.
- * @param setIsDropDownShow 필수) isDropDownShow 상태값을 set하는 함수입니다.
- * @param triggerId 필수) 드롭다운 컴포넌트가 isShow=true가 되도록 해주는 HTMLElement의 id입니다.
- * @param width 선택) width값으로 DropDown의 크기를 설정합니다. string 타입이며 기본값으로 12rem이 들어갑니다.
+ * @param bundleId 필수) 꾸러미의 고유한 id값입니다. number타입입니다.
+ * @param isShow 필수) SharedBundleDropDown가 보여지는 유무를 나타내는 상태값입니다.
+ * @param setIsShow 필수) isShow 상태값을 set하는 함수입니다.
+ * @param closeDropDown 필수) 드롭다운의 닫히는 함수를 받습니다.
+ * @param direction 필수) 드롭다운이 위치할 위치를 받습니다.
+ * @param onAddBundle 필수) 꾸러미를 추가할 때 호출되는 함수입니다.
  * @returns
  */
 
 const SharedBundleDropDown = ({
-  isDropDownShow,
-  setIsDropDownShow,
-  triggerId,
-  width = '12rem',
+  isShow,
+  setIsShow,
+  closeDropDown,
+  direction,
+  onAddBundle,
 }: SharedBundleDropDownProps) => {
   const location = useLocation();
   const SERVICE_URL = window.location.host;
   const CURRENT_URL = location.pathname;
+
+  const toastClipBoard = () => {
+    notify({
+      type: 'success',
+      text: '클립보드에 링크가 복사되었습니다.',
+    });
+  };
 
   const OPTIONS = [
     {
@@ -46,8 +56,8 @@ const SharedBundleDropDown = ({
         </IconWrapper>
       ),
       handler: () => {
-        console.log('꾸러미가 추가되었습니다'); // 추후 실제 api 연동 과정이 필요
-        setIsDropDownShow(!isDropDownShow);
+        onAddBundle();
+        setIsShow(false);
       },
     },
     {
@@ -60,23 +70,28 @@ const SharedBundleDropDown = ({
       ),
       handler: () => {
         handleCopyClipBoard(SERVICE_URL, CURRENT_URL);
-        setIsDropDownShow(!isDropDownShow);
+        toastClipBoard();
+        setIsShow(false);
       },
     },
   ];
 
   return (
-    <SharedBundleDropDownWrapper $width={width}>
-      <DropDown
-        mode="normal"
-        width={width}
-        height="fit-content"
-        options={OPTIONS}
-        isShow={isDropDownShow}
-        setIsShow={setIsDropDownShow}
-        triggerId={triggerId}
-      />
-    </SharedBundleDropDownWrapper>
+    <>
+      <div>
+        <DropDown
+          mode="normal"
+          width={12}
+          optionHeight={5}
+          height={10}
+          closeDropDown={closeDropDown}
+          options={OPTIONS}
+          isShow={isShow}
+          setIsShow={setIsShow}
+          direction={direction}
+        />
+      </div>
+    </>
   );
 };
 
