fae7f8209e6e7bf8136c2e9198f754f2d4daf79a
SoJuSo <asdfg7123@gmail.com>
2024-03-06 01:26:54 +0900

    ♻️refactor : Report페이지 상수 코드 분리 및 적용

--- src/pages/ReportPage/Report.const.ts
+++ src/pages/ReportPage/Report.const.ts
@@ -1,1 +1,31 @@
-// const
+export const ReportPageConstants = {
+  SUBMIT_CONFIRM_MESSAGE: '제출하시겠습니까?',
+
+  NOTIFY_REPORT_SUCCESS_MESSAGE:
+    '신고를 완료하였습니다. 불편을 드려 죄송합니다.',
+
+  REPORT_TITLE: '📄문의하기 페이지입니다.',
+
+  REPORT_GUIDE_ERROR_TITLE: '🚨신고',
+  REPORT_GUIDE_ERROR_TEXT:
+    '불쾌한 콘텐츠 혹은 부적절한 행동을 목격하셨나요? 저희에게 알려주시면 최대한 신속하게 처리됩니다.',
+
+  REPORT_GUIDE_SUGGEST_TITLE: '📝건의',
+  REPORT_GUIDE_SUGGEST_TEXT:
+    '플랫폼을 더 나은 곳으로 만들기 위해 여러분의 의견을 기다립니다. 기능 추가, 사용성 향상, 새로운 아이디어와 같은 모든 의견을 항상 환영합니다.',
+
+  REPORT_GUIDE_ASK_TITLE: '🛎️문의',
+  REPORT_GUIDE_ASK_TEXT:
+    '추가적인 도움이 필요하거나 질문이 있으신가요? 언제든지 문의해주세요. 최대한 빠르게 도와드릴게요.',
+
+  TEXTAREA_PLACEHOLDER: '내용을 작성해주세요.',
+  SUBMIT_BUTTON_TEXT: '제출하기',
+};
+
+export const ReportContentValidation = {
+  required: { value: true, message: '문의 내역을 작성해주세요.' },
+  minLength: {
+    value: 10,
+    message: '문의 내역은 10자 이상 작성해주세요.',
+  },
+};
--- src/pages/ReportPage/Report.tsx
+++ src/pages/ReportPage/Report.tsx
@@ -5,6 +5,8 @@ import Button from '@/components/common/Button';
 import Textarea from '@/components/common/Textarea';
 import { notify } from '@/hooks/toast';
 
+import { ReportContentValidation, ReportPageConstants } from './Report.const';
+
 const Report = () => {
   const {
     register,
@@ -20,7 +22,7 @@ const Report = () => {
   }) => {
     // todo 문의 제출 API 연동
     // API fail 에러는 현재 로직에서 처리함
-    if (window.confirm('제출하시겠습니까?')) {
+    if (window.confirm(ReportPageConstants.SUBMIT_CONFIRM_MESSAGE)) {
       console.log(createReport);
     } else {
       return;
@@ -29,7 +31,7 @@ const Report = () => {
     // 성공시
     notify({
       type: 'default',
-      text: '신고를 완료하였습니다. 불편을 드려 죄송합니다.',
+      text: ReportPageConstants.NOTIFY_REPORT_SUCCESS_MESSAGE,
     });
 
     // 홈으로
@@ -50,7 +52,7 @@ const Report = () => {
 
   const handleKeyPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key === 'Enter' && !e.shiftKey) {
-      e.preventDefault(); // 엔터 키의 기본 동작을 막습니다.
+      e.preventDefault();
       handleSubmitDebounce();
     }
   };
@@ -69,8 +71,12 @@ const Report = () => {
         }}
       >
         <div>
-          <h2 style={{ fontSize: '3rem' }}>📄문의하기 페이지입니다.</h2>
-          <span style={{ fontSize: '2rem', fontWeight: '700' }}>🚨신고</span>
+          <h2 style={{ fontSize: '3rem' }}>
+            {ReportPageConstants.REPORT_TITLE}
+          </h2>
+          <span style={{ fontSize: '2rem', fontWeight: '700' }}>
+            {ReportPageConstants.REPORT_GUIDE_ERROR_TITLE}
+          </span>
           <p
             style={{
               fontSize: '1.6rem',
@@ -78,12 +84,13 @@ const Report = () => {
               lineHeight: 'normal',
             }}
           >
-            불쾌한 콘텐츠 혹은 부적절한 행동을 목격하셨나요? 저희에게 알려주시면
-            최대한 신속하게 처리됩니다.
+            {ReportPageConstants.REPORT_GUIDE_ERROR_TEXT}
           </p>
         </div>
         <div>
-          <span style={{ fontSize: '2rem', fontWeight: '700' }}>📝건의</span>
+          <span style={{ fontSize: '2rem', fontWeight: '700' }}>
+            {ReportPageConstants.REPORT_GUIDE_SUGGEST_TITLE}
+          </span>
           <p
             style={{
               fontSize: '1.6rem',
@@ -91,13 +98,13 @@ const Report = () => {
               lineHeight: 'normal',
             }}
           >
-            플랫폼을 더 나은 곳으로 만들기 위해 여러분의 의견을 기다립니다. 기능
-            추가, 사용성 향상, 새로운 아이디어와 같은 모든 의견을 항상
-            환영합니다.
+            {ReportPageConstants.REPORT_GUIDE_SUGGEST_TEXT}
           </p>
         </div>
         <div>
-          <span style={{ fontSize: '2rem', fontWeight: '700' }}>🛎️문의</span>
+          <span style={{ fontSize: '2rem', fontWeight: '700' }}>
+            {ReportPageConstants.REPORT_GUIDE_ASK_TITLE}
+          </span>
           <p
             style={{
               fontSize: '1.6rem',
@@ -105,8 +112,7 @@ const Report = () => {
               lineHeight: 'normal',
             }}
           >
-            추가적인 도움이 필요하거나 질문이 있으신가요? 언제든지 문의해
-            주세요. 최대한 빠르게 도와드릴게요.
+            {ReportPageConstants.REPORT_GUIDE_ASK_TEXT}
           </p>
         </div>
         <form
@@ -121,17 +127,11 @@ const Report = () => {
           }}
         >
           <Textarea
-            {...register('createReport', {
-              required: { value: true, message: '문의 내역을 작성해주세요.' },
-              minLength: {
-                value: 10,
-                message: '문의 내역은 10자 이상 작성해주세요.',
-              },
-            })}
+            {...register('createReport', ReportContentValidation)}
             style={{ height: '30rem' }}
             width="100%"
             height="100%"
-            placeholder="내용을 작성해주세요."
+            placeholder={ReportPageConstants.TEXTAREA_PLACEHOLDER}
             errorMessage={
               errors.createReport?.message ? errors.createReport?.message : ''
             }
@@ -141,7 +141,7 @@ const Report = () => {
             style={{ marginTop: '1rem' }}
             width="100%"
             height="4rem"
-            text="제출하기"
+            text={ReportPageConstants.SUBMIT_BUTTON_TEXT}
             type="submit"
           />
         </form>
