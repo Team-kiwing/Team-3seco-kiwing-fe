72421394c40c34b92c8a9a4a320b90998bdbd6de
Kyungbin <pkb8839@naver.com>
2024-03-07 04:45:18 +0900

    [#162] 공유된 꾸러미 페이지 구현 (#191)
    * 🎨style : Tags 관련 타입수정
    
    - tags의 api가 변경됨에 따라 tags 타입정의도 수정하였습니다.
    - 기존의 content => name으로 변경하였습니다.
    
    * 💄design : 모바일 TagFilter 스타일 수정
    
    모바일 768px일때 태그필터 margin값 수정
    
    * ✨feat : tags API 연동 및 Selector 코드 수정
    
    실제 tags의 API연동을 진행하였습니다.
    Selector의 버전이 바뀌면서 코드 또한 수정하였습니다.
    
    * ♻️refactor : TagFilter 타입 수정
    
    * ✨feat : useBundleFilter, useBundleDisplay hook 구현
    
    - useBundleDisplay는 현재 어떤 꾸머리를 랜더링 해줄지 판단하는 함수입니다.
    - useBundleFilter 태그필터에서 선택한 꾸러미를 보여주는 함수입니다.
    
    * ✨feat : 생성된 시간 정렬으로 수정
    
    정렬을 update기준이 아닌 create기준으로 변경하였습니다.
    
    * ✨feat : sharedPage 타입 정의
    
    공유된 꾸러미에서 사용되는 타입을 정의했습니다.
    
    * ✨feat : hook을 이용하여 수정
    
    기존의 sharedPage를 추가한 hook을 이용하여 다시 구현하였습니다.
    
    * 📝docs : 필요없는 타입 삭제
    
    * ✨feat : ReactQuery이용하여 필요한 hook 구현
    
    - 디폴트값인 최근 만들어진 전체를 불러오는 useLatestBundles
    - 인기있는 전체를 불러오는 usePopularBundles
    - 선택한 태그를 최근순으로 불러오는 useLatestSelectedTagBundles
    - 선택한 태그를 인기순으로 불러오는 usePopularSelectedTagBundles
    - 검색한 키워드를 최근순으로 불러오는 useLatestSearchedBundles
    - 검색한 키워드를 인기순으로 불러오는 usePopularSearchedBundles
    위에 것들을 리액트쿼리를 이용하여 훅으로 만들었습니다.
    
    * ✨feat : react-hook사용하여 재구현
    
    * ♻️refactor : 2가지 훅으로 수정
    
    많은 훅들을 최적화하고 줄여서 2가지의 훅으로 만들었습니다.
    1.useLatestBundles 검색필터의 결과를 최신순으로 불러옵니다.
    2.usePopularBundles 검색필터의 결과를 인기순으로 불러옵니다.
    
    * ✨feat : sharedPage 페이지 구현
    
    * 🎨style : useFetchTag hook 사용
    
    useFetchTag hook을 이용하여 tag들을 불러오는 코드로 바꾸었습니다.
    불필요한 콘솔로그를 삭제하였습니다.
    
    * 💄design : sharedPage 스타일링 변경
    
    전체 컨테이너에서 gap을 웹과 모바일을 다르게 주었습니다.
    
    * 🎨style : API 타입으로 수정
    
    * 🎨style : 불필요한 코드 삭제

--- src/pages/SharedPage/Shared.tsx
+++ src/pages/SharedPage/Shared.tsx
@@ -1,11 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 
 import BundleCard from '@/components/common/BundleCard';
 import SearchBar from '@/components/common/SearchBar';
 import Selector from '@/components/common/Selector';
 import TagFilter from '@/components/common/TagFilter';
+import { useFetchTags } from '@/hooks/useFetchTags';
+import { Tag } from '@/types';
 
 import { SelectorConstants } from './Shared.const';
+import { useLatestBundles, usePopularBundles } from './Shared.hook';
 import {
   CardWrapper,
   SearchWrapper,
@@ -13,44 +17,29 @@ import {
   SharedWrapper,
   TagFilterWrapper,
 } from './Shared.style';
-import { BundleProps, TagProps } from './Shared.type';
 
 const Shared = () => {
-  const [selectedTags, setSelectedTags] = useState<TagProps[]>([]);
   const [isRecent, setIsRecent] = useState<boolean>(true);
-  const [bundles, setBundles] = useState<BundleProps[]>([]);
+  const { data: tags } = useFetchTags();
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [searchedBundles, setSearchedBundles] = useState<string>('');
 
-  useEffect(() => {
-    fetch('/bundles')
-      .then((res) => res.json())
-      .then((data) => setBundles(data));
-  }, []);
-  if (bundles.length === 0) {
-    return null;
-  }
+  const tagsId = selectedTags.map((tag) => tag.id);
+  const methods = useForm({ mode: 'onSubmit' });
+  const REGISTER = 'searchBundle';
 
-  const tagList = [
-    // 나중에 api 로직으로 대체
-    { id: 1, content: '개발자' },
-    { id: 2, content: '백엔드' },
-    { id: 3, content: '서버' },
-    { id: 4, content: '시스템' },
-    { id: 5, content: '보안' },
-    { id: 6, content: '프론트엔드' },
-    { id: 7, content: '머신러닝' },
-    { id: 8, content: 'QA' },
-    { id: 9, content: '안드로이드' },
-    { id: 10, content: '크로스플랫폼앱' },
-    { id: 11, content: '데이터' },
-    { id: 12, content: 'SW' },
-    { id: 13, content: 'DevOps' },
-    { id: 14, content: 'DBA' },
-    { id: 15, content: '네트워크' },
-    { id: 16, content: '웹퍼블리셔' },
-    { id: 17, content: '임베디드' },
-    { id: 18, content: 'IOS' },
-    { id: 19, content: 'HW' },
-  ];
+  const onSubmit = () => {
+    const searchedBundles = methods.getValues(REGISTER).trim();
+    if (searchedBundles !== '') {
+      setSearchedBundles(searchedBundles);
+    } else {
+      setSearchedBundles('');
+    }
+  };
+
+  const { data: latestBundles } = useLatestBundles(tagsId, searchedBundles);
+
+  const { data: popularBundles } = usePopularBundles(tagsId, searchedBundles);
 
   return (
     <>
@@ -59,33 +48,64 @@ const Shared = () => {
           <TagFilter
             selectedTags={selectedTags}
             setSelectedTags={setSelectedTags}
-            tagList={tagList}
+            tagList={tags ?? []}
           />
         </TagFilterWrapper>
         <SearchWrapper>
-          <SearchBar />
+          <FormProvider {...methods}>
+            <SearchBar
+              fontSize={1.8}
+              handleSearchSubmit={onSubmit}
+              REGISTER={REGISTER}
+            />
+          </FormProvider>
         </SearchWrapper>
         <SelectorWrapper>
           <Selector
             isState={isRecent}
+            setIsState={setIsRecent}
             content={[
               SelectorConstants.RECENT_SORT_TEXT,
               SelectorConstants.POPULAR_SORT_TEXT,
             ]}
-            onSelected={setIsRecent}
           />
         </SelectorWrapper>
         <CardWrapper>
-          {bundles.map((bundle) => (
-            <BundleCard
-              key={bundle.id}
-              id={bundle.id}
-              bundleName={bundle.name}
-              hashTags={bundle.tags}
-              isHot={bundle.isHot}
-              subscribedCount={bundle.scrapeCount}
-            />
-          ))}
+          {isRecent ? (
+            latestBundles?.content.length !== 0 ? (
+              latestBundles?.content.map(
+                (bundle) =>
+                  bundle.shareType === 'PUBLIC' && (
+                    <BundleCard
+                      key={bundle.id}
+                      id={bundle.id}
+                      bundleName={bundle.name}
+                      hashTags={bundle.tags}
+                      isHot={bundle.isHot}
+                      subscribedCount={bundle.scrapeCount}
+                    />
+                  )
+              )
+            ) : (
+              <h1>검색된 결과가 없습니다.</h1>
+            )
+          ) : popularBundles?.content.length !== 0 ? (
+            popularBundles?.content.map(
+              (bundle) =>
+                bundle.shareType === 'PUBLIC' && (
+                  <BundleCard
+                    key={bundle.id}
+                    id={bundle.id}
+                    bundleName={bundle.name}
+                    hashTags={bundle.tags}
+                    isHot={bundle.isHot}
+                    subscribedCount={bundle.scrapeCount}
+                  />
+                )
+            )
+          ) : (
+            <h1>검색된 결과가 없습니다.</h1>
+          )}
         </CardWrapper>
       </SharedWrapper>
     </>
