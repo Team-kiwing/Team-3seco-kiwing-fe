42154a8b556775138d8b6884c6f570223b25f167
Kyungbin <pkb8839@naver.com>
2024-03-08 01:55:01 +0900

    ✨feat : 상세 꾸러미 페이지 구현
    상세 꾸러미 페이지를 구현하였습니다.
    - api연동을 하였습니다.
    - 클립보드, 꾸러미 추가, 질문가져오기 등등 가능합니다.

--- src/pages/SharedItemPage/SharedItem.tsx
+++ src/pages/SharedItemPage/SharedItem.tsx
@@ -1,51 +1,23 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
 
 import UserInfoCard from '@/components/common/UserInfoCard';
 import SharedBundleBox from '@/components/SharedBundle/SharedBundleBox';
 import SharedBundleCard from '@/components/SharedBundle/SharedBundleCard';
-import { MOBILE } from '@/constants';
 import { getUserInfo } from '@/services/members';
-import { Col } from '@/styles/globalStyles';
 import { UserInfoResponse } from '@/types';
 
 import { useGetBundleDetail } from './SharedItem.hook';
-
-const SharedItemWrapper = styled(Col)`
-  width: 70%;
-  align-items: center;
-  margin: 0 auto;
-  gap: 3rem;
-  @media screen and (max-width: ${MOBILE}px) {
-    width: 100%;
-    gap: 1.5rem;
-  }
-`;
-
-const UserInfoWrapper = styled.div`
-  margin-top: 5rem;
-  width: 100%;
-  @media screen and (max-width: ${MOBILE}px) {
-    margin-top: 1rem;
-  }
-`;
-
-const SharedWrapper = styled(Col)`
-  width: 100%;
-  align-items: center;
-  gap: 3rem;
-  @media screen and (max-width: ${MOBILE}px) {
-    width: 90%;
-    gap: 1.5rem;
-  }
-`;
+import {
+  SharedItemWrapper,
+  SharedWrapper,
+  UserInfoWrapper,
+} from './SharedItem.style';
 
 const SharedItem = () => {
   const params = useParams();
   const bundleId = Number(params.id);
   const { data: detail } = useGetBundleDetail({ bundleId: bundleId });
-
   const [user, setUser] = useState<UserInfoResponse | null>(null);
 
   useEffect(() => {
