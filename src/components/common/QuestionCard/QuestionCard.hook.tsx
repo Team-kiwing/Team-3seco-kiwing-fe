0cabd166c47edf7262be1e3e5d097527b22bdd96
SoJuSo <asdfg7123@gmail.com>
2024-03-07 10:52:11 +0900

    🐛fix : 오타 수정

--- src/components/common/QuestionCard/QuestionCard.hook.tsx
+++ src/components/common/QuestionCard/QuestionCard.hook.tsx
@@ -1,7 +1,7 @@
 import { useModal } from '@/hooks/useModal';
 
 import { QuestionCardConstants } from './QuestionCard.const';
-import { QuestionCardModal } from './QuestionCard.modal';
+import { QuestionCardModal } from './QuestionCard.Modal';
 
 export const useReportModal = () => {
   const { setModalOpen } = useModal();
