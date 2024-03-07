f1894c13eef1c68d48867b5f94e7841eb32898fb
SoJuSo <asdfg7123@gmail.com>
2024-03-07 16:00:49 +0900

    ✨feat : 태그 필터 및 레이아웃 추가

--- src/pages/HubPage/Hub.style.ts
+++ src/pages/HubPage/Hub.style.ts
@@ -1,1 +1,10 @@
-// style
+import { styled } from 'styled-components';
+
+import { Col } from '@/styles/globalStyles';
+
+export const HubLayout = styled(Col)`
+  width: 90%;
+  align-items: center;
+  margin: 0 auto;
+  padding: 1rem 0;
+`;
--- src/pages/HubPage/Hub.tsx
+++ src/pages/HubPage/Hub.tsx
@@ -1,8 +1,25 @@
+import { useState } from 'react';
+
+import TagFilter from '@/components/common/TagFilter';
+import { useFetchTags } from '@/hooks/useFetchTags';
+import { Tag } from '@/types';
+
+import { HubLayout } from './Hub.style';
+
 const Hub = () => {
+  const { data: tags } = useFetchTags();
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
   // 요기다가 하시면 됩니다
   return (
     <>
-      <div>Hub 입니다.</div>
+      <HubLayout>
+        <TagFilter
+          selectedTags={selectedTags}
+          setSelectedTags={setSelectedTags}
+          tagList={tags ?? []}
+        />
+      </HubLayout>
     </>
   );
 };
