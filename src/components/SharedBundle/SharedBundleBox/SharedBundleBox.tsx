20fa35e1835d6b0183bf924b5ea46ff34436a09e
Kyungbin <pkb8839@naver.com>
2024-03-07 11:01:29 +0900

    ✨feat : SharedBundleBox 컴포넌트 구현

--- src/components/SharedBundle/SharedBundleBox/SharedBundleBox.tsx
+++ src/components/SharedBundle/SharedBundleBox/SharedBundleBox.tsx
@@ -10,6 +10,10 @@ import SharedQuestionBox from '../SharedQuestionBox';
 import { CheckBoxInput } from '../SharedQuestionBox/SharedQuestionBox.style';
 import { SHARED_BUNDLE_BOX } from './SharedBundleBox.const';
 import {
+  useCreateQuestionsToBundle,
+  useGetMyBundles,
+} from './SharedBundleBox.hook';
+import {
   AllCheckWrapper,
   CountText,
   SharedBundleBoxFooter,
@@ -29,107 +33,53 @@ const SharedBundleBox = ({ questions }: SharedBundleBoxProps) => {
     useDropDown('sharedBundle-dropdown');
 
   const [checkedAllItems, setCheckedAllItems] = useState(false);
-  const [checkedItems, setCheckedItems] = useState<number[]>([]);
-  const myBundles = [
-    // 추후에 API 내꾸러미 불러오기 로직으로 변경
-    {
-      id: 1,
-      name: '데브코스 면접리스트',
-      shareType: 'string',
-      isHot: true,
-      createdAt: '2024-02-28T00:22:09.752Z',
-      updatedAt: '2024-02-28T00:22:09.752Z',
-    },
-    {
-      id: 2,
-      name: '삼성 면접리스트',
-      shareType: 'string',
-      isHot: true,
-      createdAt: '2024-02-28T00:22:09.752Z',
-      updatedAt: '2024-02-28T00:22:09.752Z',
-    },
-    {
-      id: 4,
-      name: '카카오 면접리스트',
-      shareType: 'string',
-      isHot: true,
-      createdAt: '2024-02-28T00:22:09.752Z',
-      updatedAt: '2024-02-28T00:22:09.752Z',
-    },
-    {
-      id: 5,
-      name: '네이버 면접리스트',
-      shareType: 'string',
-      isHot: true,
-      createdAt: '2024-02-28T00:22:09.752Z',
-      updatedAt: '2024-02-28T00:22:09.752Z',
-    },
-    {
-      id: 6,
-      name: '쿠팡 면접리스트',
-      shareType: 'string',
-      isHot: true,
-      createdAt: '2024-02-28T00:22:09.752Z',
-      updatedAt: '2024-02-28T00:22:09.752Z',
-    },
-  ];
-
-  // function checkQuestion(questionId: number[], checkedItems: number[]) {
-  //   // 추후에 중복 질문 로직 연동예정
-  //   return checkedItems.some((originId) => questionId.includes(originId));
-  // }
+  const [checkedQuestionId, setCheckedQuestionId] = useState<number[]>([]);
+  const { data: getMyBundles } = useGetMyBundles('LATEST');
+  const { mutate } = useCreateQuestionsToBundle();
 
   const handleToggleCheck = (id: number, isChecked: boolean) => {
     if (isChecked) {
-      setCheckedItems([...checkedItems, id]);
+      setCheckedQuestionId([...checkedQuestionId, id]);
     } else {
-      setCheckedItems(checkedItems.filter((item) => item !== id));
+      setCheckedQuestionId(checkedQuestionId.filter((item) => item !== id));
     }
   };
 
   const handleAllCheck = () => {
     setCheckedAllItems(!checkedAllItems);
     if (!checkedAllItems) {
-      const allId = questions.map((item) => item.id);
-      setCheckedItems(allId);
+      const allCheckedId = questions.map((item) => item.id);
+      setCheckedQuestionId(allCheckedId);
     } else {
-      setCheckedItems([]);
+      setCheckedQuestionId([]);
     }
   };
 
-  const handleAddQuestion = (checkedBundleId: number[]) => {
-    if (checkedBundleId) {
-      if (checkedItems.length == 0) {
-        notify({
-          type: 'error',
-          text: SHARED_BUNDLE_BOX.FAIL_QUESTION_NOTIFY,
-        });
-      } else {
-        if (checkedBundleId.length == 0) {
-          notify({
-            type: 'warning',
-            text: SHARED_BUNDLE_BOX.WARNING_BUNDLE_NOTIFY,
-          });
-        } else {
-          notify({
-            type: 'success',
-            text: SHARED_BUNDLE_BOX.SUCCESS_QUESTION_NOTIFY,
-          });
-          console.log(
-            `선택한 BundleId가 ${checkedBundleId}인 질문꾸러미에 체크 된 QuestionId ${checkedItems}를 가진 질문을 추가합니다.`
-          );
-          setCheckedItems([]);
-          setCheckedAllItems(false);
-        }
-      }
+  const handleAddQuestion = (checkedBundleId: number | null) => {
+    if (checkedBundleId == null) {
+      return;
+    }
+    if (checkedQuestionId.length == 0) {
+      notify({
+        type: 'warning',
+        text: SHARED_BUNDLE_BOX.FAIL_QUESTION_NOTIFY,
+      });
+    } else {
+      mutate({ bundleId: checkedBundleId, questionIds: checkedQuestionId });
+      setCheckedQuestionId([]);
+      setCheckedAllItems(false);
+      notify({
+        type: 'success',
+        text: SHARED_BUNDLE_BOX.SUCCESS_QUESTION_NOTIFY,
+      });
     }
   };
 
-  const BUNDLE_BOX_OPTIONS = myBundles.map((item) => ({
-    id: item.id,
-    title: item.name,
-    body: '', // 추후에 중복 질문 로직 연동예정
-  }));
+  const BUNDLE_BOX_OPTIONS =
+    getMyBundles?.map((item) => ({
+      id: item.id,
+      title: item.name,
+    })) ?? [];
 
   return (
     <>
@@ -159,7 +109,7 @@ const SharedBundleBox = ({ questions }: SharedBundleBoxProps) => {
               question={item?.content}
               answer={item?.answer}
               questionId={item?.id}
-              isChecked={checkedItems.includes(item?.id)}
+              isChecked={checkedQuestionId.includes(item?.id)}
               onToggleCheck={handleToggleCheck}
             />
           ))}
@@ -176,7 +126,7 @@ const SharedBundleBox = ({ questions }: SharedBundleBoxProps) => {
             <DropDown
               width={20}
               optionHeight={5}
-              height={4.7 * BUNDLE_BOX_OPTIONS.length}
+              height={15}
               options={BUNDLE_BOX_OPTIONS}
               isShow={isShow}
               closeDropDown={closeDropDown}
