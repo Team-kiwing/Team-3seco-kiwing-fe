a66e79692375ab8feb8eb7099498f24bd865b308
SoJuSo <asdfg7123@gmail.com>
2024-03-08 22:28:26 +0900

    ✨feat : SearchBar 추가 WIP

--- src/pages/HubPage/Hub.style.ts
+++ src/pages/HubPage/Hub.style.ts
@@ -3,7 +3,8 @@ import { styled } from 'styled-components';
 import { Col } from '@/styles/globalStyles';
 
 export const HubLayout = styled(Col)`
-  width: 90%;
+  width: 70%;
+  max-width: 1200px;
   align-items: center;
   margin: 0 auto;
   padding: 1rem 0;
--- src/pages/HubPage/Hub.tsx
+++ src/pages/HubPage/Hub.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 
+import SearchBar from '@/components/common/SearchBar';
 import TagFilter from '@/components/common/TagFilter';
 import { useFetchTags } from '@/hooks/useFetchTags';
 import { Tag } from '@/types';
@@ -7,18 +9,36 @@ import { Tag } from '@/types';
 import { HubLayout } from './Hub.style';
 
 const Hub = () => {
+  console.log(`hubPage render`);
   const { data: tags } = useFetchTags();
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
 
-  // 요기다가 하시면 됩니다
+  const methods = useForm({ mode: 'onBlur' });
+
+  const REGISTER = 'hubSearch';
+
+  const onSubmit = () => {
+    // 검색 시 실행할 함수를 입력합니다.
+    console.log(methods.getValues(REGISTER));
+  };
+
   return (
     <>
       <HubLayout>
         <TagFilter
+          style={{ marginTop: '2rem' }}
           selectedTags={selectedTags}
           setSelectedTags={setSelectedTags}
           tagList={tags ?? []}
         />
+        <FormProvider {...methods}>
+          <SearchBar
+            // fontSize={2}
+            handleSearchSubmit={onSubmit}
+            maxWidth={'500px'}
+            REGISTER={REGISTER}
+          />
+        </FormProvider>
       </HubLayout>
     </>
   );
