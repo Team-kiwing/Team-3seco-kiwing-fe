22c8aef40545a49531200d5ee97535d164e11aa5
SeokJu Kim <asdfg7123@gmail.com>
2024-03-08 10:05:36 +0900

    [#187] 문의 페이지 v1.0.0 (#220)
    * ✨feat : 문의, 신고 페이지 뼈대 파일 구조 생성
    
    * 🐛fix : 사소한 오탈자로 인한 에러 수정
    
    * ✨feat : 문의 페이지 기본 기능 코드 추가
    
    * ♻️refactor : Report페이지 상수 코드 분리 및 적용
    
    * ♻️refactor : 스타일링 코드 분리, 적용
    
    * 💄design : 모바일 환경 대응
    
    * ♻️refactor : 반복되는 코드 로직을 map 함수로 리팩토링
    
    * ✨feat : 타입 파일 분리
    
    * 🗑️remove : 미사용 파일 제거
    
    * ✨feat : hook요소 파일 분리 및 적용
    
    * 💡comment : Todo 사항 명시
    
    * 🐛fix : onChange to onBlur로 Validation 로직 최적화
    
    * 💄design : 전역 선언 스타일 적용

---
+++ src/pages/ReportPage/Report.style.ts
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+import {
+  FONT_BOLD,
+  FONT_MEDIUM,
+  MOBILE,
+  MOBILE_FONT_SIZE,
+  WEB_FONT_SIZE,
+} from '@/constants';
+import { Col } from '@/styles/globalStyles';
+
+export const ReportPageLayout = styled(Col)`
+  width: 50%;
+  margin: 0 auto;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+
+  color: ${({ theme }) => theme.primary_color};
+
+  @media screen and (max-width: ${MOBILE}px) {
+    width: 90%;
+    height: auto;
+    padding-bottom: 1rem;
+  }
+`;
+
+export const ReportGuideContainer = styled(Col)`
+  width: 100%;
+`;
+
+export const ReportPageTitle = styled.h2`
+  font-size: 3rem;
+
+  @media screen and (max-width: ${MOBILE}px) {
+    font-size: 2rem;
+  }
+`;
+
+export const ReportGuideTitle = styled.span`
+  font-size: 2rem;
+  font-weight: ${FONT_BOLD};
+
+  @media screen and (max-width: ${MOBILE}px) {
+    font-size: 1.5rem;
+  }
+`;
+
+export const ReportGuideText = styled.p`
+  font-size: ${WEB_FONT_SIZE}rem;
+  font-weight: ${FONT_MEDIUM};
+  line-height: normal;
+  word-break: keep-all;
+
+  @media screen and (max-width: ${MOBILE}px) {
+    word-break: break-all;
+    font-size: ${MOBILE_FONT_SIZE}rem;
+  }
+`;
+
+export const ReportForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: auto;
+`;
--- src/pages/ReportPage/index.tsx
+++ src/pages/ReportPage/index.tsx
@@ -1,10 +1,1 @@
-const ReportPage = () => {
-  // 요기다가 하시면 됩니다
-  return (
-    <>
-      <div>ReportPage 입니다.</div>
-    </>
-  );
-};
-
-export default ReportPage;
+export { default } from './Report';
