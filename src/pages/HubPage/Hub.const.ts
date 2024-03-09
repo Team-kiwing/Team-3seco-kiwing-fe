201e285545bbce4dcd534bac9b727b1ed9c5215e
SoJuSo <asdfg7123@gmail.com>
2024-03-10 01:54:59 +0900

    ✨feat : 질문 허브 페이지에서 동작하는 상수 파일 정의 및 적용

--- src/pages/HubPage/Hub.const.ts
+++ src/pages/HubPage/Hub.const.ts
@@ -1,1 +1,11 @@
-// const
+export const HubHookConstants = {
+  HUB_SEARCH_REGISTER: 'hubSearch',
+  HUB_SELECTOR_CONTENT: ['최근순', '스크랩 순'],
+  HUB_DYNAMIC_QUERY_KEY: (
+    keyword: string,
+    filterdTags: object | string,
+    isRecent: string
+  ) => 'questionSearch' + keyword + filterdTags + isRecent.toString(),
+  HUB_TAG_FILTERING: (tags: number[]) =>
+    tags.length === 0 ? [].join('') : tags.join(', '),
+};
