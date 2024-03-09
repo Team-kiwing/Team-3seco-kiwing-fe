cf5b7077f83bdca1665a9e1be5d5c156f48f1f26
SoJuSo <asdfg7123@gmail.com>
2024-03-10 01:55:28 +0900

    ✨feat : 스타일링 작업, 기타 세부 기능 추가 WIP
    서버작업으로 인한 작업 커밋입니다.

--- src/pages/HubPage/Hub.tsx
+++ src/pages/HubPage/Hub.tsx
@@ -1,25 +1,49 @@
 import { useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 
+import QuestionCard from '@/components/common/QuestionCard';
 import SearchBar from '@/components/common/SearchBar';
+import Selector from '@/components/common/Selector';
 import TagFilter from '@/components/common/TagFilter';
 import { useFetchTags } from '@/hooks/useFetchTags';
-import { Tag } from '@/types';
+import { userDataStore } from '@/stores';
+import { Question, SortingType, Tag } from '@/types';
 
+import { HubHookConstants } from './Hub.const';
+import { useSearchQuestionsInfinite } from './Hub.hook';
 import { HubLayout } from './Hub.style';
 
 const Hub = () => {
-  console.log(`hubPage render`);
-  const { data: tags } = useFetchTags();
+  console.log(`hubPage render`, new Date());
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [isRecent, setIsRecent] = useState<SortingType>('LATEST');
+  const [searchParams, setSearchParams] = useState('');
+  const { data: tags } = useFetchTags();
+  const { isLogin } = userDataStore();
 
   const methods = useForm({ mode: 'onBlur' });
+  const tagsId = selectedTags.map((tag) => tag.id);
 
-  const REGISTER = 'hubSearch';
+  const {
+    data: infinityData,
+    isFetching,
+    isFetchingNextPage,
+    isError,
+    hasNextPage,
+    fetchNextPage,
+  } = useSearchQuestionsInfinite(searchParams, isRecent, tagsId);
+
+  console.log(infinityData, hasNextPage);
 
   const onSubmit = () => {
-    // 검색 시 실행할 함수를 입력합니다.
-    console.log(methods.getValues(REGISTER));
+    const searchValues = methods
+      .getValues(HubHookConstants.HUB_SEARCH_REGISTER)
+      .trim();
+    if (searchValues !== '') {
+      setSearchParams(searchValues);
+    } else {
+      setSearchParams('');
+    }
   };
 
   return (
@@ -33,12 +57,43 @@ const Hub = () => {
         />
         <FormProvider {...methods}>
           <SearchBar
-            // fontSize={2}
             handleSearchSubmit={onSubmit}
             maxWidth={'500px'}
-            REGISTER={REGISTER}
+            REGISTER={HubHookConstants.HUB_SEARCH_REGISTER}
+          />
+          <Selector
+            isState={isRecent === 'LATEST'}
+            content={HubHookConstants.HUB_SELECTOR_CONTENT}
+            setIsState={(item: boolean) =>
+              setIsRecent(item ? 'LATEST' : 'POPULAR')
+            }
           />
         </FormProvider>
+        {isFetching && <div>로딩즁</div>}
+        {isError && <div>에러남!</div>}
+        {isFetchingNextPage && <div>다음 페이지 불러오는 즁</div>}
+
+        {infinityData
+          ? infinityData.pages.map((pageList) => {
+              return pageList?.questionResponses.map(
+                (questionItem: Question) => (
+                  <QuestionCard
+                    key={questionItem.id}
+                    tags={questionItem.tags}
+                    id={questionItem.id}
+                    content={questionItem.content}
+                    shareCount={questionItem.shareCount}
+                    isHot={questionItem.isHot}
+                    isLogin={isLogin}
+                  />
+                )
+              );
+            })
+          : null}
+
+        {hasNextPage && (
+          <button onClick={() => fetchNextPage()}>다음 페이지 불러오기</button>
+        )}
       </HubLayout>
     </>
   );
