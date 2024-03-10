b241a024043312acc83d442db69b679eb5b97eb3
SoJuSo <asdfg7123@gmail.com>
2024-03-11 00:32:23 +0900

    ✨feat : QuestionCard에서 사용하는 API 호출 쿼리문 정의

--- src/components/common/QuestionCard/QuestionCard.hook.tsx
+++ src/components/common/QuestionCard/QuestionCard.hook.tsx
@@ -1,16 +1,69 @@
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+import { notify } from '@/hooks/toast';
 import { useModal } from '@/hooks/useModal';
+import { createQuestionsToBundle, getMyBundles } from '@/services/bundles';
 
 import { QuestionCardConstants } from './QuestionCard.const';
 import { QuestionCardModal } from './QuestionCard.Modal';
 
-export const useReportModal = () => {
+export const useReportModal = ({ questionId }: { questionId: number }) => {
   const { setModalOpen } = useModal();
   const handleReportClick = () => {
     setModalOpen({
       title: QuestionCardConstants.MODAL_TITLE,
-      content: <QuestionCardModal />,
+      content: <QuestionCardModal questionId={questionId} />,
     });
   };
 
   return { handleReportClick };
+};
+
+interface BundleParsed {
+  id: number;
+  name: string;
+}
+
+export const useGetMyBundles = (sortingType: string) => {
+  const query = useQuery<BundleParsed[] | null>({
+    queryKey: ['myBundles'],
+    queryFn: () => getMyBundles(sortingType),
+    select: (data) => {
+      return data
+        ? data.map(
+            (bundleList): BundleParsed => ({
+              id: bundleList.id,
+              name: bundleList.name,
+            })
+          )
+        : null;
+    },
+    enabled: false,
+  });
+
+  return query;
+};
+
+interface Props {
+  ids: number[];
+  checkedBundles: number[];
+}
+
+export const useCreateQuestionsToBundle = () => {
+  const mutation = useMutation({
+    mutationFn: ({ ids, checkedBundles }: Props) =>
+      createQuestionsToBundle({ questionIds: ids, bundleIds: checkedBundles }),
+    onError: () => {
+      notify({ type: 'error', text: '에러가 발생했어요.' });
+    },
+    onSuccess: (res) => {
+      if (res) {
+        notify({ type: 'success', text: '추가 완료!' });
+      } else {
+        notify({ type: 'error', text: '에러가 발생했어요.' });
+      }
+    },
+  });
+
+  return mutation;
 };
