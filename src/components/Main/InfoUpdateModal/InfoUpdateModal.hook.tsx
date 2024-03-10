31cd301da9aa184c311a7f093a3bb9448a46c493
JaeHyunGround <gothddlek@naver.com>
2024-03-11 00:26:51 +0900

    ✨feat : 모달에서 쓰일 사용자 정보 수정 hook 정의

---
+++ src/components/Main/InfoUpdateModal/InfoUpdateModal.hook.tsx
@@ -0,0 +1,34 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { notify } from '@/hooks/toast';
+import { useModal } from '@/hooks/useModal';
+import { patchMyInfo, updateProfileImage } from '@/services/members';
+import { ImageRequest, UserInfoRequest, UserInfoResponse } from '@/types';
+
+export const useUpdateMyInfo = () => {
+  const { setModalCompleteClose } = useModal();
+  return useMutation({
+    mutationFn: ({ nickname, snsRequests, tagIds }: UserInfoRequest) =>
+      patchMyInfo({ nickname, snsRequests, tagIds }),
+    onSuccess: (res: UserInfoResponse | string) => {
+      if (typeof res === 'string') {
+        if (res.includes('400')) {
+          notify({
+            type: 'warning',
+            text: '사용자 아이디가 중복됐습니다.',
+          });
+        }
+      } else {
+        setModalCompleteClose();
+        window.location.reload();
+      }
+    },
+    onError: () => {},
+  });
+};
+
+export const useUpdateImage = () => {
+  return useMutation({
+    mutationFn: ({ file }: ImageRequest) => updateProfileImage({ file }),
+  });
+};
