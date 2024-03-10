859ea3ff0ed4722f491e73ee0fefbb3e9ab613cb
SoJuSo <asdfg7123@gmail.com>
2024-03-11 01:00:58 +0900

    ✨feat : 문의 페이지 API 연동

--- src/pages/ReportPage/Report.hook.ts
+++ src/pages/ReportPage/Report.hook.ts
@@ -1,10 +1,36 @@
+import { useMutation } from '@tanstack/react-query';
 import { debounce } from 'lodash';
 import { SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 import { notify } from '@/hooks/toast';
+import { createClaim } from '@/services/etc';
 
 import { ReportPageConstants } from './Report.const';
 import { ReportSubmitProps } from './Report.type';
+
+export const useReportQuestion = () => {
+  const navigator = useNavigate();
+  const mutation = useMutation({
+    mutationFn: ({ content }: { content: string }) => createClaim({ content }),
+    onError: () => {
+      notify({ type: 'error', text: '에러가 발생했어요.' });
+    },
+    onSuccess: (res) => {
+      if (res) {
+        notify({
+          type: 'default',
+          text: '문의를 완료하였습니다. 최대한 빠르게 반영할게요!',
+        });
+        navigator('/');
+      } else {
+        notify({ type: 'error', text: '에러가 발생했어요.' });
+      }
+    },
+  });
+
+  return mutation;
+};
 
 export const useReportForm = () => {
   const {
@@ -15,15 +41,11 @@ export const useReportForm = () => {
     mode: 'onBlur',
     defaultValues: { createReport: '' },
   });
+  const { mutate } = useReportQuestion();
 
   const onValid: SubmitHandler<ReportSubmitProps> = ({ createReport }) => {
     if (window.confirm(ReportPageConstants.SUBMIT_CONFIRM_MESSAGE)) {
-      // todo API 연동
-      console.log(createReport);
-      notify({
-        type: 'default',
-        text: ReportPageConstants.NOTIFY_REPORT_SUCCESS_MESSAGE,
-      });
+      mutate({ content: createReport });
     } else {
       return;
     }
