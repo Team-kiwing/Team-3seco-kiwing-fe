42a65e76e9463bd921079212e4ee218066a27a15
Kyungbin <pkb8839@naver.com>
2024-03-10 23:31:43 +0900

    ✨feat : sharedPage 구현

--- src/pages/SharedPage/Shared.tsx
+++ src/pages/SharedPage/Shared.tsx
@@ -2,31 +2,37 @@ import { useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 
 import BundleCard from '@/components/common/BundleCard';
+import Button from '@/components/common/Button';
 import SearchBar from '@/components/common/SearchBar';
 import Selector from '@/components/common/Selector';
+import Spinner from '@/components/common/Spinner';
 import TagFilter from '@/components/common/TagFilter';
 import { useFetchTags } from '@/hooks/useFetchTags';
-import { Tag } from '@/types';
+import { BundlesBasic, SortingType, Tag } from '@/types';
 
-import { SelectorConstants } from './Shared.const';
-import { useLatestBundles, usePopularBundles } from './Shared.hook';
+import { SharedHookConstants, SharedTextConstants } from './Shared.const';
+import { useSearchBundlesInfinite } from './Shared.hook';
 import {
   CardWrapper,
   SearchWrapper,
   SelectorWrapper,
+  SharedFooterWrapper,
+  SharedInfiniteMessage,
+  SharedSearchError,
+  SharedSearchNone,
   SharedWrapper,
   TagFilterWrapper,
 } from './Shared.style';
 
 const Shared = () => {
-  const [isRecent, setIsRecent] = useState<boolean>(true);
+  const [isRecent, setIsRecent] = useState<SortingType>('LATEST');
   const { data: tags } = useFetchTags();
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
   const [searchedBundles, setSearchedBundles] = useState<string>('');
 
   const tagsId = selectedTags.map((tag) => tag.id);
   const methods = useForm({ mode: 'onSubmit' });
-  const REGISTER = 'searchBundle';
+  const REGISTER = SharedHookConstants.SHARED_SEARCH_REGISTER;
 
   const onSubmit = () => {
     const searchedBundles = methods.getValues(REGISTER).trim();
@@ -37,9 +43,14 @@ const Shared = () => {
     }
   };
 
-  const { data: latestBundles } = useLatestBundles(tagsId, searchedBundles);
-
-  const { data: popularBundles } = usePopularBundles(tagsId, searchedBundles);
+  const {
+    data: infinityData,
+    isFetching,
+    isFetchingNextPage,
+    isError,
+    hasNextPage,
+    fetchNextPage,
+  } = useSearchBundlesInfinite(tagsId, searchedBundles, isRecent);
 
   return (
     <>
@@ -62,51 +73,73 @@ const Shared = () => {
         </SearchWrapper>
         <SelectorWrapper>
           <Selector
-            isState={isRecent}
-            setIsState={setIsRecent}
+            isState={isRecent === 'LATEST'}
+            setIsState={(isRecent) =>
+              setIsRecent(isRecent ? 'LATEST' : 'POPULAR')
+            }
             content={[
-              SelectorConstants.RECENT_SORT_TEXT,
-              SelectorConstants.POPULAR_SORT_TEXT,
+              SharedHookConstants.SHARED_SELECTOR_CONTENT[0],
+              SharedHookConstants.SHARED_SELECTOR_CONTENT[1],
             ]}
           />
         </SelectorWrapper>
+        {isFetching && <Spinner />}
         <CardWrapper>
-          {isRecent ? (
-            latestBundles?.content.length !== 0 ? (
-              latestBundles?.content.map(
-                (bundle) =>
+          {infinityData &&
+            infinityData.pages.map((pageList) => {
+              return pageList?.content.map(
+                (bundle: BundlesBasic) =>
                   bundle.shareType === 'PUBLIC' && (
                     <BundleCard
                       key={bundle.id}
-                      id={bundle.id}
                       bundleName={bundle.name}
                       hashTags={bundle.tags}
-                      isHot={bundle.isHot}
+                      id={bundle.id}
                       subscribedCount={bundle.scrapeCount}
+                      isHot={bundle.isHot}
                     />
                   )
-              )
-            ) : (
-              <h1>검색된 결과가 없습니다.</h1>
-            )
-          ) : popularBundles?.content.length !== 0 ? (
-            popularBundles?.content.map(
-              (bundle) =>
-                bundle.shareType === 'PUBLIC' && (
-                  <BundleCard
-                    key={bundle.id}
-                    id={bundle.id}
-                    bundleName={bundle.name}
-                    hashTags={bundle.tags}
-                    isHot={bundle.isHot}
-                    subscribedCount={bundle.scrapeCount}
-                  />
-                )
-            )
-          ) : (
-            <h1>검색된 결과가 없습니다.</h1>
-          )}
+              );
+            })}
+          {!isFetching &&
+            !isFetchingNextPage &&
+            infinityData?.pages[0]?.content.length === 0 && (
+              <SharedSearchNone>
+                {SharedTextConstants.SHARED_SEARCH_NONE}
+              </SharedSearchNone>
+            )}
         </CardWrapper>
+
+        <SharedFooterWrapper>
+          {hasNextPage && !isFetchingNextPage && (
+            <Button
+              width="100%"
+              height="3rem"
+              onClick={() => fetchNextPage()}
+              text={SharedTextConstants.SHARED_NEXT_PAGE_BUTTON}
+            />
+          )}
+          {infinityData?.pages[0]?.content.length &&
+          !hasNextPage &&
+          !isFetchingNextPage ? (
+            <Button
+              width="100%"
+              height="3rem"
+              disabled
+              text={SharedTextConstants.SHARED_NEXT_PAGE_NONE_BUTTON}
+            />
+          ) : null}
+          {isFetching && isFetchingNextPage && (
+            <SharedInfiniteMessage>
+              {SharedTextConstants.SHARED_INFINITY_LOADING}
+            </SharedInfiniteMessage>
+          )}
+        </SharedFooterWrapper>
+        {isError && (
+          <SharedSearchError>
+            {SharedTextConstants.SHARED_ERROR_MESSAGE}
+          </SharedSearchError>
+        )}
       </SharedWrapper>
     </>
   );
