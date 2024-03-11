cbf4f5fd35834ab5c6db04edcb3a085f8078e256
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-03-11 12:00:49 +0900

    [#210] RegisterPage v1.1.0 (#259)
    * ✨feat : 최초 로그인 한 사용자의 access, refresh token 저장
    
    * 🗑️remove : Register.type.ts 제거
    
    $isMobile props가 없어짐에 따라 Register.type.ts 파일을 삭제했습니다.
    
    * 💄design : &isMobile이 없어짐에 따른 media 쿼리 적용
    
    * 💄design : 로고 위치 가운데 정렬
    
    * ♻️refactor : Input 제거 및 placeholder, label 수정
    
    - 이름 input 제거
    - 링크 placeholder를 각각 GitHub, 블로그, 기타로 수정
    - 사용자 이름 label 대신 닉네임 label로 수정
    
    * 💄design : margin-bottom 값 수정
    
    * 💄design : 인사 문구 로고 위쪽으로 위치 변경
    
    * ♻️refactor : 이용약관 링크 a 태그 대신 useNavigate를 사용하여 링크 이동 처리
    
    * 💄design : cursor: pointer 추가
    
    * ✨feat : react-hook-form을 위한 타입 추가
    
    * ✨feat : react-hook-form 적용 wip
    
    * ✨feat : myinfo 쿼리키 추가
    
    내 정보를 불러오는 react-query를 관리하기 위해 쿼리 키를 추가했습니다.
    
    * ✨feat : 내 정보 업데이트 API hook 생성
    
    * ♻️refactor : 필요없는 코드 제거 (myinfo 쿼리 키)
    
    * ✨feat : Register 페이지 기능 추가
    
    - 가입 버튼을 눌렀을 때 회원 가입이 되는 로직을 구현했습니다.
    - 한 번 서비스에 가입 한 사람이거나 로그인 한 사람들은 Register 페이지에 접근하지 못하도록 막아주는 방어 로직을 구현했습니다.
    - 태그 필터에 3개까지 추가할 수 있도록 제한을 두었습니다.
    
    * 💡comment : 닉네임 중복 처리 필요 TODO 작성
    
    * 🐛fix : 회원가입 과정에서 닉네임 입력 안하고 메인으로 갔을 때 상황을 방지
    
    * ✨feat : 이용약관 모달 추가
    
    * 🐛fix : 회원가입 통신 로직 수정
    
    * ♻️refactor : PR 리뷰 반영
    
    * 💄design : 인트로 레이아웃 수정
    
    ---------
    
    Co-authored-by: SoJuSo <asdfg7123@gmail.com>

--- src/pages/RegisterPage/Register.type.ts
+++ src/pages/RegisterPage/Register.type.ts
@@ -1,3 +1,7 @@
-export interface RegisterPageStyleProps {
-  $isMobile?: boolean;
+export interface RegisterForm {
+  email: string;
+  nickname: string;
+  github: string;
+  blog: string;
+  etc: string;
 }
