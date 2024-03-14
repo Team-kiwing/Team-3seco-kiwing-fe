efafbd5aa2421f3ffba2fce7237c6306a31369b3
Kyungbin <pkb8839@naver.com>
2024-03-14 17:46:02 +0900

    ✨feat : NoSearchResults 컴포넌트 구현
    NoSearchResults 컴포넌트를 구현했습니다.
    사용방법은 jsDoc으로 작성하였습니다.

--- src/components/common/NoSearchResults/NoSearchResults.tsx
+++ src/components/common/NoSearchResults/NoSearchResults.tsx
@@ -1,5 +1,39 @@
-const NoSearchResults = () => {
-  return <div />;
+import {
+  NO_SEARCH_RESULTS_ALT_IMAGE,
+  NO_SEARCH_RESULTS_IMAGE,
+} from './NoSearchResults.const';
+import {
+  NoSearchResultsImages,
+  NotSearchWrapper,
+  Text1,
+  Text2,
+} from './NoSearchResults.style';
+import { NoSearchResultsProps } from './NoSearchResults.type';
+
+/**
+ * @summary 사용법   <NoSearchResults
+            text1="앗"
+            text2="검색된 결과가 없습니다."
+          />
+ * @description 공통 NoSearchResults 컴포넌트
+ * @param text1 메인 텍스트 string
+ * @param text2 서브 텍스트 string
+ * @returns
+ */
+
+const NoSearchResults = ({ text1, text2 }: NoSearchResultsProps) => {
+  return (
+    <>
+      <NotSearchWrapper>
+        <Text1>{text1}</Text1>
+        <Text2>{text2}</Text2>
+        <NoSearchResultsImages
+          src={NO_SEARCH_RESULTS_IMAGE}
+          alt={NO_SEARCH_RESULTS_ALT_IMAGE}
+        />
+      </NotSearchWrapper>
+    </>
+  );
 };
 
 export default NoSearchResults;
