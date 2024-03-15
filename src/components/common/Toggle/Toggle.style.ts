e4493b3cd7722b0960aef21ca0479fbca53a2243
kim-hyunjoo <khj9x.x@gmail.com>
2024-03-15 10:52:17 +0900

    💄design : isBackgroundGray props 통해 배경이 더 어두워지도록 디자인 변경

--- src/components/common/Toggle/Toggle.style.ts
+++ src/components/common/Toggle/Toggle.style.ts
@@ -18,11 +18,13 @@ export const ToggleSwitch = styled.div<ToggleSwitchProps>`
   width: 100%;
   height: 100%;
 
-  border: 0.2rem solid
-    ${(props) => (props.$isBorderShow ? props.theme.gray_500 : 'transparent')};
+  border: 0.2rem solid transparent;
 
   border-radius: ${({ $height }) => $height};
-  background-color: ${(props) => props.theme.container_color};
+  background-color: ${(props) =>
+    props.$isBackgroundGray
+      ? props.theme.gray_300
+      : props.theme.container_color};
   transition: all 0.2s ease;
   position: relative;
   color: ${(props) => props.theme.gray_600};
