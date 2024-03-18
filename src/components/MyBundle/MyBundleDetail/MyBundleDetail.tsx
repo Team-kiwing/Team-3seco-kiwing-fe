bc1ab481df53135cce66e43d601001f3768320dd
kim-hyunjoo <khj9x.x@gmail.com>
2024-03-18 21:47:53 +0900

    💄design : 화살표 아이콘 cursor: auto로 변경

--- src/components/MyBundle/MyBundleDetail/MyBundleDetail.tsx
+++ src/components/MyBundle/MyBundleDetail/MyBundleDetail.tsx
@@ -195,6 +195,9 @@ const MyBundleDetail = ({
                   <IconWrapper
                     $size={'l'}
                     $fillColor={theme.gray_300}
+                    style={{
+                      cursor: 'auto',
+                    }}
                   >
                     <MdOutlineKeyboardDoubleArrowDown />
                   </IconWrapper>
